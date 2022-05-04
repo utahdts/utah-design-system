4e131ad365580edc2bafc828d492f78558d01119
Austin Haws <ahaws@utah.gov>
2022-05-04 16:39:08 -0600

    Serve on unique port

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 9180
+  },
+  preview: {
+    port: 8080
+  }
 })
