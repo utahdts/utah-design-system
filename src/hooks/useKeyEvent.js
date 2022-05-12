ec0e94c77c98d7a13bc425bb7cc14f7fd41c5827
Austin Haws <ahaws@utah.gov>
2022-05-12 10:46:37 -0600

    Update useCalback for simple, readable, correct

--- src/hooks/useKeyEvent.js
+++ src/hooks/useKeyEvent.js
@@ -1,9 +1,8 @@
-import { useCallback } from 'react';
-import intersection from 'lodash/intersection';
 import identity from 'lodash/identity';
+import intersection from 'lodash/intersection';
 
 // these match event's altKey, ctrlKey, metaKey, shiftKey names
-export const MODIFIER_KEYS = {
+export const EVENT_MODIFIERS = {
   ALT: 'alt',
   CTRL: 'ctrl',
   META: 'meta',
@@ -11,22 +10,23 @@ export const MODIFIER_KEYS = {
 };
 
 /**
- * @param codes (array): the string codes for keyboard keys. ie ['Enter', 'Return', 'Tab', ...]
- * @param handler (function): function to call when key is detected; will be passed the event. ie (event) => ... do something with event ...
- * @param modifiers (array) : the modifier prefix(es) to match when detecting keys. ie ['alt', 'ctrl', ...] (can use MODIFIER_KEYS enum)
+ * @param codes (array): (optional) the string codes for keyboard keys. ie ['Enter', 'Return', 'Tab', ...]
+ * @param handler (function): (required) function to call when key is detected; will be passed the event. ie (event) => ...do something with event...
+ * @param modifiers (array) : (optional) the event's modifier prefix(es) to match. ie ['alt', 'ctrl', ...] (should use EVENT_MODIFIERS enum)
  */
-export default ({ codes, handler, modifiers }) => (
-  useCallback(
-    (event) => {
-      const eventCodes = [event.code, event.keyCode, event.key];
-      const eventModifiers = Object.values(MODIFIER_KEYS).map((modifier) => (event[`${modifier}Key`] ? modifier : null))
-        .filter(identity);
+export default ({ codes = [], handler, modifiers = [] }) => (
+  (event) => {
+    const eventCodes = [event.code, event.keyCode, event.key];
+    const eventModifiers = Object.values(EVENT_MODIFIERS)
+      .map((modifier) => (event[`${modifier}Key`] ? modifier : null))
+      .filter(identity);
 
-      // find matching values between the event and the component's desired keys
-      if (intersection(eventCodes, codes).length && (!modifiers.length || intersection(eventModifiers, modifiers).length)) {
-        handler(event);
-      }
-    },
-    [...codes]
-  )
+    // find matching codes/modifiers between the event and the passed in codes/modifiers
+    if (
+      (!codes.length || intersection(eventCodes, codes).length)
+      && (!modifiers.length || intersection(eventModifiers, modifiers).length)
+    ) {
+      handler(event);
+    }
+  }
 );
