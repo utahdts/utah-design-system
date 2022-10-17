0c2d93de216bf2b753e1aac5865a384c9b84277e
Joseph Sharp <jmsharp@utah.gov>
2022-10-17 15:05:23 -0600

    Remove extra div elements from button example props

--- website/src/react/components/websiteContent/library/components/buttons/ButtonExampleProps.jsx
+++ website/src/react/components/websiteContent/library/components/buttons/ButtonExampleProps.jsx
@@ -37,63 +37,59 @@ function ButtonExampleProps({ setState, state }) {
   );
 
   return (
-    <div>
-      <div>
-        <Form
-          // onSubmit(({ state, validationErrors }) => ... do whatever ...)
-          state={state}
-          setState={setState}
-        >
-          <Select id="props.appearance" label="Appearance" className="input--height-small1x">
-            <SelectOption label="Outlined" value="outlined" />
-            <SelectOption label="Solid" value="solid" />
-          </Select>
+    <Form
+      // onSubmit(({ state, validationErrors }) => ... do whatever ...)
+      state={state}
+      setState={setState}
+      className="form--stacked"
+    >
+      <Select id="props.appearance" label="Appearance" className="input--height-small1x">
+        <SelectOption label="Outlined" value="outlined" />
+        <SelectOption label="Solid" value="solid" />
+      </Select>
+      <Switch id="props.isBusy" label="Busy" />
 
-          <Switch id="props.isBusy" label="Busy" />
+      <TextInput id="props.className" label="Class" className="input--height-small1x" />
 
-          <TextInput id="props.className" label="Class" className="input--height-small1x" />
+      <Select id="props.color" label="Color" className="input--height-small1x">
+        <SelectOption label="Primary" value="primary" />
+        <SelectOption label="Secondary" value="secondary" />
+        <SelectOption label="Accent" value="accent" />
+        <SelectOption label="None" value="none" />
+      </Select>
 
-          <Select id="props.color" label="Color" className="input--height-small1x">
-            <SelectOption label="Primary" value="primary" />
-            <SelectOption label="Secondary" value="secondary" />
-            <SelectOption label="Accent" value="accent" />
-            <SelectOption label="None" value="none" />
-          </Select>
+      <Switch id="props.isDisabled" label="Disabled" width={20} />
 
-          <Switch id="props.isDisabled" label="Disabled" width={20} />
+      <Select id="props.iconLeft" label="Icon Left" className="input--height-small1x">
+        <SelectOption label="Chevron" value="IconChevron" />
+        <SelectOption label="Arrow" value="IconArrowLeft" />
+        <SelectOption label="None" value="none" />
+      </Select>
 
-          <Select id="props.iconLeft" label="Icon Left" className="input--height-small1x">
-            <SelectOption label="Chevron" value="IconChevron" />
-            <SelectOption label="Arrow" value="IconArrowLeft" />
-            <SelectOption label="None" value="none" />
-          </Select>
+      <Select id="props.iconRight" label="Icon Right" className="input--height-small1x">
+        <SelectOption label="Chevron" value="IconChevron" />
+        <SelectOption label="Arrow" value="IconArrowRight" />
+        <SelectOption label="None" value="none" />
+      </Select>
 
-          <Select id="props.iconRight" label="Icon Right" className="input--height-small1x">
-            <SelectOption label="Chevron" value="IconChevron" />
-            <SelectOption label="Arrow" value="IconArrowRight" />
-            <SelectOption label="None" value="none" />
-          </Select>
+      <TextInput id="props.id" label="ID" className="input--height-small1x" />
 
-          <TextInput id="props.id" label="ID" className="input--height-small1x" />
+      <Select id="props.size" label="Size" className="input--height-small1x">
+        <SelectOption label="Small" value={formElementSizesEnum.SMALL} />
+        <SelectOption label="Medium" value={formElementSizesEnum.MEDIUM} />
+        <SelectOption label="Large" value={formElementSizesEnum.LARGE} />
+        <SelectOption label="Large 1X" value={formElementSizesEnum.LARGE1X} />
+      </Select>
 
-          <Select id="props.size" label="Size" className="input--height-small1x">
-            <SelectOption label="Small" value={formElementSizesEnum.SMALL} />
-            <SelectOption label="Medium" value={formElementSizesEnum.MEDIUM} />
-            <SelectOption label="Large" value={formElementSizesEnum.LARGE} />
-            <SelectOption label="Large 1X" value={formElementSizesEnum.LARGE1X} />
-          </Select>
+      <TextInput id="props.title" label="Title (children)" className="input--height-small1x" />
 
-          <TextInput id="props.title" label="Title (children)" className="input--height-small1x" />
+      <Select id="props.type" label="Type" className="input--height-small1x">
+        <SelectOption label="Button" value="button" />
+        <SelectOption label="Reset" value="reset" />
+        <SelectOption label="Submit" value="submit" />
+      </Select>
 
-          <Select id="props.type" label="Type" className="input--height-small1x">
-            <SelectOption label="Button" value="button" />
-            <SelectOption label="Reset" value="reset" />
-            <SelectOption label="Submit" value="submit" />
-          </Select>
-
-        </Form>
-      </div>
-    </div>
+    </Form>
   );
 }
 
