ce84aa5d851bf62081ed8570c3b6eaff586194d4
Austin Haws <ahaws@utah.gov>
2022-12-13 13:35:09 -0700

    Check for .local. for blank baseName

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -10,6 +10,7 @@ if (window.location.href.includes('/dev/')) {
 } else if (
   window.location.href.includes('localhost')
   || window.location.href.includes('127.0.0.1')
+  || window.location.href.includes('.local.')
 ) {
   baseName = '';
 } else {
