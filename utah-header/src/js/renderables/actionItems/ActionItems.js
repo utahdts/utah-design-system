6d0f4ab09fab929b34c4e6b0cbddce55d5dbfc5b
Joseph Sharp <jmsharp@utah.gov>
2023-01-05 08:11:32 -0700

    Style action items and add icons

--- utah-header/src/js/renderables/actionItems/ActionItems.js
+++ utah-header/src/js/renderables/actionItems/ActionItems.js
@@ -5,25 +5,33 @@ import ActionItemsWrapper from './html/ActionItemsWrapper.html?raw';
 import ActionItem from './html/ActionItem.html?raw';
 // eslint-disable-next-line import/no-unresolved
 import WaffleIcon from '../icons/html/Waffle.html?raw';
+// eslint-disable-next-line import/no-unresolved
+import QuestionIcon from '../icons/html/QuestionIcon.html?raw';
+// eslint-disable-next-line import/no-unresolved
+import AlertIcon from '../icons/html/AlertIcon.html?raw';
 
-function actionItem({ title, icon }) {
+function actionItem({ title, icon, className }) {
   const actionItemElement = renderDOM(ActionItem);
   const titleElement = document.createTextNode(title);
   actionItemElement.querySelector('.utds-header-action-item__title').appendChildAll(titleElement);
-  actionItemElement.querySelector('.utds-header-action-item__icon-button').appendChildAll(renderDOM(icon));
+  const iconButton = actionItemElement.querySelector('.utds-header-action-item__icon-button');
+  if (className) {
+    iconButton.classList.add(className);
+  }
+  iconButton.appendChildAll(renderDOM(icon));
   return actionItemElement;
 }
 
 function waffleActionItem() {
-  return actionItem({ title: 'Waffle', icon: WaffleIcon });
+  return actionItem({ title: 'Waffle', icon: WaffleIcon, className: 'icon-waffle' });
 }
 
 function helpActionItem() {
-  return actionItem({ title: 'Help', icon: WaffleIcon });
+  return actionItem({ title: 'Help', icon: QuestionIcon });
 }
 
 function alertsActionItem() {
-  return actionItem({ title: 'Alerts', icon: WaffleIcon });
+  return actionItem({ title: 'Alerts', icon: AlertIcon });
 }
 
 export default function ActionItems() {
