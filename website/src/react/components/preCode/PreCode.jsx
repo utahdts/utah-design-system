b2d3b98b85acbecc53d7b51197e9cf6e4248aeea
Austin Haws <ahaws@utah.gov>
2023-01-26 09:01:17 -0700

    Add PreCode components to show DOM string on page
    Co-authored-by: Joseph Sharp <jmsharp-utah@users.noreply.github.com>

---
+++ website/src/react/components/preCode/PreCode.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import cleanHtmlForInnerHTML from './cleanHtmlForInnerHTML';
+
+const propTypes = {
+  // the raw unformatted DOM code string (probably from an innerHTML?)
+  codeRaw: PropTypes.string.isRequired,
+};
+const defaultProps = {};
+
+function PreCode({ codeRaw }) {
+  const [codeInnerHtml, setCodeInnerHtml] = useState(null);
+
+  useEffect(
+    () => {
+      setCodeInnerHtml(cleanHtmlForInnerHTML(codeRaw));
+    },
+    [codeRaw]
+  );
+
+  return (
+    <div className="gimme-some-class">
+      <pre>
+        <code
+          className="language-xml"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: codeInnerHtml }}
+        />
+      </pre>
+    </div>
+  );
+}
+
+PreCode.propTypes = propTypes;
+PreCode.defaultProps = defaultProps;
+
+export default PreCode;
--- website/src/react/components/websiteContent/resources/IconsDocumentation/Icons/IconsDocumentation.jsx
+++ website/src/react/components/websiteContent/resources/IconsDocumentation/Icons/IconsDocumentation.jsx
@@ -1,177 +1,20 @@
+import { useRef } from 'react';
 import { useImmer } from 'use-immer';
+import PreCodeForRef from '../../../../preCode/PreCodeForRef';
 import StaticExample from '../../../../staticExamples/StaticExample';
+import iconsDocumentationIcons from './iconsDocumentationIcons';
 
 const propTypes = {};
 const defaultProps = {};
 
 /* eslint-disable react/jsx-one-expression-per-line */
 function IconsDocumentation() {
-  const iconsList = [
-    {
-      cssClass: 'ext-link',
-      title: 'external link',
-      letter: 'A',
-    },
-    {
-      cssClass: 'waffle',
-      title: 'waffle',
-      letter: 'B',
-    },
-    {
-      cssClass: 'alert',
-      title: 'alert',
-      letter: 'C',
-    },
-    {
-      cssClass: 'help',
-      title: 'help',
-      letter: 'D',
-    },
-    {
-      cssClass: 'bookmark',
-      title: 'bookmark',
-      letter: 'E',
-    },
-    {
-      cssClass: 'search',
-      title: 'search',
-      letter: 'F',
-    },
-    {
-      cssClass: 'check',
-      title: 'checkmark',
-      letter: 'G',
-    },
-    {
-      cssClass: 'star',
-      title: 'star',
-      letter: 'H',
-    },
-    {
-      cssClass: 'info',
-      title: 'info',
-      letter: 'I',
-    },
-    {
-      cssClass: 'unfold-less',
-      title: 'unfold less',
-      letter: 'J',
-    },
-    {
-      cssClass: 'unfold-more',
-      title: 'unfold more',
-      letter: 'K',
-    },
-    {
-      cssClass: 'circle-chevron-down',
-      title: 'circle chevron down',
-      letter: 'L',
-    },
-    {
-      cssClass: 'circle-chevron-up',
-      title: 'circle chevron up',
-      letter: 'M',
-    },
-    {
-      cssClass: 'chevron-up',
-      title: 'chevron up',
-      letter: 'N',
-    },
-    {
-      cssClass: 'chevron-right',
-      title: 'chevron right',
-      letter: 'O',
-    },
-    {
-      cssClass: 'chevron-down',
-      title: 'chevron down',
-      letter: 'P',
-    },
-    {
-      cssClass: 'chevron-left',
-      title: 'chevron left',
-      letter: 'Q',
-    },
-    {
-      cssClass: 'arrow-up',
-      title: 'arrow up',
-      letter: 'R',
-    },
-    {
-      cssClass: 'arrow-right',
-      title: 'arrow right',
-      letter: 'S',
-    },
-    {
-      cssClass: 'arrow-down',
-      title: 'arrow down',
-      letter: 'T',
-    },
-    {
-      cssClass: 'arrow-left',
-      title: 'arrow left',
-      letter: 'U',
-    },
-    {
-      cssClass: 'plus',
-      title: 'plus',
-      letter: 'V',
-    },
-    {
-      cssClass: 'minus',
-      title: 'minus',
-      letter: 'W',
-    },
-    {
-      cssClass: 'x-icon',
-      title: 'close',
-      letter: 'X',
-    },
-    {
-      cssClass: 'edit',
-      title: 'edit',
-      letter: 'Y',
-    },
-    {
-      cssClass: 'edit-box',
-      title: 'edit box',
-      letter: 'Z',
-    },
-    {
-      cssClass: 'verified',
-      title: 'verified',
-      letter: 'a',
-    },
-    {
-      cssClass: 'gear',
-      title: 'gear',
-      letter: 'b',
-    },
-    {
-      cssClass: 'doc',
-      title: 'doc',
-      letter: 'c',
-    },
-    {
-      cssClass: 'doc-square',
-      title: 'doc square',
-      letter: 'd',
-    },
-    {
-      cssClass: 'warning',
-      title: 'warning',
-      letter: 'e',
-    },
-    {
-      cssClass: 'error',
-      title: 'error',
-      letter: 'f',
-    },
-  ];
-
   const [state, setState] = useImmer({
-    currentIcon: iconsList[3],
+    currentIcon: iconsDocumentationIcons[3],
   });
+  const iconDOMRef1 = useRef();
+  const iconDOMRef2 = useRef();
+  const iconDOMRef3 = useRef();
 
   function clickIconButton(icon) {
     setState((draftState) => {
@@ -189,7 +32,7 @@ function IconsDocumentation() {
       <hr />
       <h2 id="section-example">Example</h2>
       <div className="icon-example__grid mb-spacing">
-        {iconsList.map((icon) => (
+        {iconsDocumentationIcons.map((icon) => (
           <button
             className="icon-example__item"
             key={`${icon.cssClass}-icon-button`}
@@ -208,11 +51,13 @@ function IconsDocumentation() {
         title="Accessible Icon Strategy 1"
         renderedExample={(
           <>
-            <div>
+            <div ref={iconDOMRef1}>
               <span className={`utds-icon-before-${state?.currentIcon?.cssClass}`} aria-hidden="true" />
               <span className="visually-hidden">{state?.currentIcon?.title}</span>
             </div>
-            <div>Code Example</div>
+            <div>
+              <PreCodeForRef deps={[state.currentIcon]} targetRef={iconDOMRef1} />
+            </div>
           </>
         )}
         quickTips={(
@@ -227,10 +72,12 @@ function IconsDocumentation() {
         title="Accessible Icon Strategy 2"
         renderedExample={(
           <>
-            <div>
+            <div ref={iconDOMRef2}>
               <span className={`utds-icon-before-${state?.currentIcon?.cssClass}`} role="img" title={state?.currentIcon?.title} />
             </div>
-            <div>Code Example</div>
+            <div>
+              <PreCodeForRef deps={[state.currentIcon]} targetRef={iconDOMRef2} />
+            </div>
           </>
         )}
         quickTips={(
@@ -246,11 +93,13 @@ function IconsDocumentation() {
         title="Accessible Icon Strategy 3"
         renderedExample={(
           <>
-            <div>
+            <div ref={iconDOMRef3}>
               <span className={`utds-icon-before-${state?.currentIcon?.cssClass}`} aria-hidden="true" />
               <span>{state?.currentIcon?.title.toUpperCase()}</span>
             </div>
-            <div>Code Example</div>
+            <div>
+              <PreCodeForRef deps={[state.currentIcon]} targetRef={iconDOMRef3} />
+            </div>
           </>
         )}
         quickTips={(
