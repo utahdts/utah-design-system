e48aed8975c6f15c7c8665560518171f51b2b0a5
Casey Wardle <chwardle@utah.gov>
2023-02-09 13:56:04 -0700

    remove github pages basename from main.jsx

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -4,18 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CssContextProvider } from './react/context/cssContext/CssContext';
 
-let baseName;
-if (window.location.href.includes('/dev/')) {
-  baseName = 'utah-design-system/dev';
-} else if (
-  window.location.href.includes('localhost')
-  || window.location.href.includes('127.0.0.1')
-  || window.location.href.includes('local.')
-) {
-  baseName = '';
-} else {
-  baseName = 'utah-design-system';
-}
+const baseName = '';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
