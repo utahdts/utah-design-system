07edaa51883036bcddbd734fcc6745ef2a941bd7
Aaron McElwee <amcelwee@utah.gov>
2023-02-13 16:24:10 -0700

    Update badge page definitions

--- website/src/react/components/websiteContent/library/components/widgetsIndicators/badges/BadgesDocumentation.jsx
+++ website/src/react/components/websiteContent/library/components/widgetsIndicators/badges/BadgesDocumentation.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import {
   Tab,
   TabGroup,
@@ -27,7 +28,7 @@ function BadgesDocumentation() {
   return (
     <div className="documentation-content">
       <h1 id="h1-top">Badges</h1>
-      <p className="lead-in">A <code>&lt;badge&gt;</code> element is awesome.</p>
+      <p className="lead-in">Badges convey dynamic information, such as counts or status. A badge can include labels or numbers.</p>
       <hr />
       <h2 id="section-example">Example</h2>
       <SandboxExample
@@ -49,34 +50,67 @@ function BadgesDocumentation() {
 
       <h2 id="section-guidance" className="mb-spacing">Guidance</h2>
       <h3 id="section-when-to-use">When to use</h3>
-      <ul className="mb-spacing">
-        <li><strong>Badges?! We don&apos;t need no stinkin&apos; badges.</strong> Use a badge to indicate anarchy.</li>
-      </ul>
+      <p>
+        Badges are used to indicate a notification, item count, or other information relating to a navigation destination.
+        Typically they are placed on the upper right corner of a component such as an icon button.
+      </p>
+
+      <p>There are two types of badges:</p>
+      <ol>
+        <li><strong>Small Badge (shape only, no characters)</strong>: A small badge is a simple circle, used to indicate a status change or an unread notification.</li>
+        <li><strong>Large Badge (usually contain numbers)</strong>: A large badge may contain numbers communicating item count information.</li>
+      </ol>
 
       <h3 id="section-when-to-use-something-else">When to use something else</h3>
       <ul className="mb-spacing">
-        <li><strong>always.</strong> always never use badges.</li>
+        <li><strong>If not alerting a customer to a notification change</strong>: Badges should be used to alert the user to a status change or unread notifications.</li>
+        <li><strong>It creates a conflict with another icon</strong>: If the badge overpowers the icon or creates confusion, consider changing the icon or icon size. Avoid using icons that look similar to badges.</li>
       </ul>
 
       <h3 id="section-usability">Usability guidance</h3>
       <ul className="mb-spacing">
-        <li><strong>Keep badges red.</strong> Red is a wonderful color.</li>
+        <li><strong>Convey dynamic information with navigation</strong>:  Use badges with navigation items to convey dynamic information associated with that destination.</li>
+        <li><strong>Recommended Sizing</strong>:
+          <ul>
+            <li>Small badges should be smaller than large badges since they contain no information</li>
+            <li>Large badge should be large enough for the information to be readable.</li>
+            <li>Use a “+” to indicate a count greater than the available character count.
+              <ul>
+                <li>e.g. 9+, 99+, 999+</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li><strong>Most commonly used with</strong>: Badges are most commonly used within other components, such as icon buttons, navigation elements, and tabs.</li>
+        <li><strong>Badge proximity</strong>: Keep the badge in close proximity to its parent element. When space permits, place the badge inside the parent element on the right side. (e.g. a navigation list item or regular button)</li>
+        <li><strong>Maximum character count</strong>: Ensure that badge labels don’t extend too far beyond the badge container. Typically don’t use more than 4 characters.</li>
+        <li><strong>Avoid changing the position</strong>: Badges have fixed positions. Generally, don’t change the position of the badge arbitrarily or place the badge directly over the parent component.</li>
+        <li><strong>Hide badge when acknowledged</strong>: When a badge is used to indicate an unread notification, the badge gets hidden once it has been viewed or acknowledged by the user. </li>
       </ul>
 
       <h3 id="section-accessibility" className="mb-spacing">Accessability</h3>
       <h4 id="section-contrast">Contrast</h4>
       <ul className="mb-spacing">
-        <li>The badge boundary (the outside edge of the badge) must maintain a <code>3:1</code> contrast ratio or better.</li>
+        <li>Badges use a color intended to stand out against labels, icons, and navigation elements. Typically use the default color mapping (red) to avoid color conflict issues. You may use other colors to convey different meanings.</li>
+        <li>The majority of the badge boundary must maintain a <code>3:1</code> contrast between its surrounding elements.</li>
+        <li>The badge text must maintain a <code>4.5:1</code> contrast inside the badge.</li>
       </ul>
 
       <h4 id="section-keyboard-interactivity">Keyboard interactivity</h4>
       <ul className="mb-spacing">
-        <li>The badge should display a visible focus state when users tab to them.</li>
+        <li>Badges are most commonly used within other components, such as icon buttons, navigation elements, and tabs. They rarely, if ever, receive keyboard focus.</li>
       </ul>
 
       <h4 id="section-screen-readers">Screen readers</h4>
       <ul className="mb-spacing">
-        <li>A Badge that contains an icon that adds additional information, the icon should have non-empty alt text (or aria-label for SVG).</li>
+        <li>The accessibility label for a badge item will be read after its navigation destination or button label. (e.g. Alerts, 1 unread alert; News, 5 new news items)</li>
+        <li>Large badges will have their text read. Additional contextual information visible only to the screen reader may be required to provide context.
+          <ul>
+            <li>Good: 12 Unread Messages; 5 Unread Alerts</li>
+            <li>Bad: 12; 5</li>
+          </ul>
+        </li>
+        <li>Small badges will simply announce &quot;new notification” or “notifications available”.</li>
       </ul>
 
       <h2 id="section-settings-props">Settings and Props</h2>
