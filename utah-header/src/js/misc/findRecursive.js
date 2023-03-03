2df6085d8b031426cb3fae2ca06d4becf8c6bab8
Austin Haws <ahaws@utah.gov>
2023-03-03 11:00:25 -0700

    Cleanup and test findRecursive
    UTAHDS-560 UTAHDS-563

--- utah-header/src/js/misc/findRecursive.js
+++ utah-header/src/js/misc/findRecursive.js
@@ -8,28 +8,30 @@
  * @param {T[] | T} object the object on which to start searching
  * @param {string[]} recursiveFields which fields on the object should be "dug" in to recursively
  * @param {(o: T) => boolean} isMatchFunc the function to call on each child to see if it matches
+ * @returns {boolean} true if the isMatchFunc is true for the root or any of its children
  */
 export default function findRecursive(object, recursiveFields, isMatchFunc) {
-  // TODO: move to misc folder as own file and write jest tests for it
-  // TODO: break out in to separate logic commands instead of a one-liner
-  return (
-    Array.isArray(object)
-      // if it's an array, check each item
-      ? object.some((o) => findRecursive(o, recursiveFields, isMatchFunc))
-      : (
-        // check if the current object matches
-        isMatchFunc(object)
-        // didn't match so now see if there are any children that would match
-        || (
-          recursiveFields
-            // check if the field exists on the object
-            .filter((field) => object[field])
-            // recurse on the field to check for matches further down
-            .some((field) => (
-              Array.isArray(object[field])
-                ? (object[field].some((childObject) => isMatchFunc(childObject) || findRecursive(childObject, recursiveFields, isMatchFunc)))
-                : (isMatchFunc(object) || findRecursive(object, recursiveFields, isMatchFunc))
-            ))
-        )
-      ));
+  /** @type {boolean} */
+  let result = false;
+
+  if (object) {
+    if (Array.isArray(object)) {
+      // try each item in the array separately
+      result = object.some((o) => findRecursive(o, recursiveFields, isMatchFunc));
+    } else {
+      // does this object match?
+      result = isMatchFunc(object);
+
+      if (!result) {
+        // object didn't match so try its children
+        result = (
+          !!recursiveFields
+            ?.filter((field) => object[field])
+            ?.some((field) => findRecursive(object[field], recursiveFields, isMatchFunc))
+        );
+      }
+    }
+  }
+
+  return result;
 }
---
+++ utah-header/test/js/misc/findRecursive.test.js
@@ -0,0 +1,144 @@
+import { describe, expect, test } from 'vitest';
+import findRecursive from '../../../src/js/misc/findRecursive';
+
+describe('findRecursive', () => {
+  const testObject = {
+    id: 1,
+    one: 1,
+    two: 2,
+    children: [
+      {
+        id: 2, one: 1, two: 3, children: [],
+      },
+      {
+        id: 3, one: 1, two: 4, children: null,
+      },
+      {
+        id: 4,
+        one: 1,
+        two: 2,
+        children: [
+          {
+            id: 5,
+            one: 1,
+            two: 2,
+            children: [{
+              id: 6, one: 1, two: 5,
+            }],
+          },
+          {
+            id: 7, one: 1, two: 6, children: [{ id: 8, one: 1, two: 7 }],
+          },
+          {
+            id: 24,
+            one: 10,
+            two: 20,
+            children: {
+              id: 25,
+              one: 13,
+              two: 15,
+            },
+          },
+        ],
+        children2: [
+          {
+            id: 9, one: 10, two: 2, children: [{ id: 10, one: 1, two: 5 }],
+          },
+          {
+            id: 11, one: 11, two: 6, children: [{ id: 12, one: 1, two: 7 }],
+          },
+        ],
+      },
+    ],
+    children2: [
+      {
+        id: 13, one: 1, two: 3, children: [],
+      },
+      {
+        id: 14, one: 1, two: 4, children: null,
+      },
+      {
+        id: 15,
+        one: 1,
+        two: 2,
+        children: [
+          {
+            id: 16, one: 1, two: 2, children: [{ id: 17, one: 1, two: 5 }],
+          },
+          {
+            id: 18, one: 1, two: 6, children: [{ id: 19, one: 1, two: 7 }],
+          },
+        ],
+        children2: [
+          {
+            id: 20, one: 20, two: 2, children: [{ id: 21, one: 1, two: 5 }],
+          },
+          {
+            id: 22, one: 21, two: 6, children: [{ id: 23, one: 1, two: 7 }],
+          },
+        ],
+      },
+    ],
+  };
+
+  test('matching', () => {
+    expect(findRecursive(testObject, ['children'], (o) => o.one === 1)).toBe(true);
+    expect(findRecursive(testObject, ['children'], (o) => o.one === 20)).toBe(false);
+    expect(findRecursive(testObject, ['children2'], (o) => o.one === 20)).toBe(true);
+    expect(findRecursive(testObject, ['children'], (o) => o.id === 6)).toBe(true);
+    expect(findRecursive(testObject, ['children'], (o) => o.id === 7)).toBe(true);
+    expect(findRecursive(testObject, ['children2'], (o) => o.id === 11)).toBe(false);
+    expect(findRecursive(testObject, ['children2'], (o) => o.one === 11)).toBe(false);
+  });
+
+  test('multi-fields', () => {
+    expect(findRecursive(testObject, ['children', 'children2'], (o) => o.id === 11)).toBe(true);
+    expect(findRecursive(testObject, ['children', 'children2'], (o) => o.one === 11)).toBe(true);
+  });
+
+  test('object is array', () => {
+    expect(findRecursive([
+      { testObject, id: '1a' },
+      { testObject, id: '1b' },
+      { testObject, id: '1c' },
+    ], ['children'], (o) => o.id === '1a')).toBe(true);
+
+    expect(findRecursive([
+      { testObject, id: '1a' },
+      { testObject, id: '1b' },
+      { testObject, id: '1c' },
+    ], ['children'], (o) => o.id === '1b')).toBe(true);
+
+    expect(findRecursive([
+      { testObject, id: '1a' },
+      { testObject, id: '1b' },
+      { testObject, id: '1c' },
+    ], ['children'], (o) => o.id === '1c')).toBe(true);
+
+    expect(findRecursive([
+      { testObject, id: '1a' },
+      { testObject, id: '1b' },
+      { testObject, id: '1c' },
+    ], ['children'], (o) => o.id === '1d')).toBe(false);
+  });
+
+  test('children is not array', () => {
+    expect(findRecursive(testObject, ['children'], (o) => o.id === 25)).toBe(true);
+  });
+
+  test('empty object', () => {
+    expect(findRecursive({}, ['children'], (o) => o.id === 25)).toBe(false);
+    expect(findRecursive(undefined, ['children'], (o) => o.id === 25)).toBe(false);
+    expect(findRecursive(null, ['children'], (o) => o.id === 25)).toBe(false);
+    expect(findRecursive(3, ['children'], (o) => o.id === 25)).toBe(false);
+    expect(findRecursive(NaN, ['children'], (o) => o.id === 25)).toBe(false);
+    expect(findRecursive('what a weird test', ['children'], (o) => o.id === 25)).toBe(false);
+  });
+
+  test('no fields', () => {
+    expect(findRecursive({}, [], (o) => o.id === 25)).toBe(false);
+    expect(findRecursive({}, null, (o) => o.id === 25)).toBe(false);
+    expect(findRecursive({}, undefined, (o) => o.id === 25)).toBe(false);
+    expect(findRecursive({}, ['i am not a real field'], (o) => o.id === 25)).toBe(false);
+  });
+});
