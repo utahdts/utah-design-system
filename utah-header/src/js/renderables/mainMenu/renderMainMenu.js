b556d19e8d935bd7e441682eab5d0db9cccde056
Austin Haws <ahaws@utah.gov>
2023-03-06 08:12:37 -0700

    Move main menu labeldby to the button
    Co-authored-by: Joseph Sharp <jmsharp-utah@users.noreply.github.com>
    
    UTAHDS-560 UTAHDS-573

--- utah-header/src/js/renderables/mainMenu/renderMainMenu.js
+++ utah-header/src/js/renderables/mainMenu/renderMainMenu.js
@@ -58,7 +58,6 @@ export default function renderMainMenu() {
       if (!mainMenuItemTitle) {
         throw new Error(`renderMainMenu(): sub menu title not found for ${menuItem.title}`);
       }
-      mainMenuItemTitle.setAttribute('id', `main-menu-item__${menuItem.title}-${uuidv4()}`);
 
       const mainMenuItemButtonTitle = /** @type {HTMLElement} */ (
         mainMenuItemTitle.querySelector(getCssClassSelector(domConstants.MENU_ITEM__BUTTON_TITLE))
@@ -66,6 +65,8 @@ export default function renderMainMenu() {
       if (!mainMenuItemButtonTitle) {
         throw new Error(`renderMainMenu(): button title not found for ${menuItem.title}`);
       }
+      mainMenuItemButtonTitle.setAttribute('id', `${domConstants.MENU_ITEM__BUTTON_TITLE}__${menuItem.title}-${uuidv4()}`);
+
       const mainMenuItemLinkTitle = mainMenuItemTitle.querySelector(getCssClassSelector(domConstants.MENU_ITEM__LINK_TITLE));
       if (!mainMenuItemLinkTitle) {
         throw new Error(`renderMainMenu(): link title not found for ${menuItem.title}`);
@@ -87,7 +88,7 @@ export default function renderMainMenu() {
           menuItems: menuItem.actionMenu,
           title: menuItem.title,
         };
-        const subMenuPopup = renderPopupMenu(popupMenu, mainMenuItemTitle, { childrenMenuType: childrenMenuTypes.FLYOUT });
+        const subMenuPopup = renderPopupMenu(popupMenu, mainMenuItemButtonTitle, { childrenMenuType: childrenMenuTypes.FLYOUT });
         mainMenuItem.appendChild(subMenuPopup);
         popupFocusHandler(mainMenuItem, mainMenuItemButtonTitle, subMenuPopup, 'menu', { shouldFocusOnHover: true });
       } else if (menuItem.actionFunction) {
@@ -107,49 +108,6 @@ export default function renderMainMenu() {
       }
     });
   }
-
-  /*
-  TODO: are all these things accounted for!
-  <!-- <button aria-expanded="true" aria-labelledby="menu-item-components__popups-menuHeader::Popups"
-      class="menu-item__button-title" id="menu-item-components__popups-menuHeader::Popups" type="button">Popups</button>
-    <button class="button icon-button menu-item__chevron menu-item__chevron--open icon-button--borderless" type="button"
-      aria-labelledby="menu-item-components__popups-menuHeader::Popups" aria-expanded="true">
-      <svg viewBox="0 0 10.75 6.834" class="icon-svg icon-chevron ">
-        <g>
-          <path d="M5.375,6.834L0,1.438,1.437,0l3.938,3.938L9.313,0l1.437,1.438L5.375,6.834Z"></path>
-        </g>
-      </svg>
-      <span class="visually-hidden">expand sub-menu</span></button></span>
-  <ul class="menu-item__sub-menu--open" role="menu">
-    <li class="menu-item"><span class="menu-item__title"><a aria-current="page" class="menu-item--selected"
-          href="/library/components/navigation/popups">Popups</a></span></li>
-  </ul> -->
-  */
-  /*
-<!-- <button aria-expanded="true" aria-labelledby="menu-item-components__popups-menuHeader::Popups"
-    class="menu-item__button-title" id="menu-item-components__popups-menuHeader::Popups" type="button">Popups</button>
-  <button class="button icon-button menu-item__chevron menu-item__chevron--open icon-button--borderless" type="button"
-    aria-labelledby="menu-item-components__popups-menuHeader::Popups" aria-expanded="true">
-    <svg viewBox="0 0 10.75 6.834" class="icon-svg icon-chevron ">
-      <g>
-        <path d="M5.375,6.834L0,1.438,1.437,0l3.938,3.938L9.313,0l1.437,1.438L5.375,6.834Z"></path>
-      </g>
-    </svg>
-    <span class="visually-hidden">expand sub-menu</span></button></span>
-<ul class="menu-item__sub-menu--open" role="menu">
-  <li class="menu-item"><span class="menu-item__title"><a aria-current="page" class="menu-item--selected"
-        href="/library/components/navigation/popups">Popups</a></span></li>
-</ul> -->
-<!-- a vertical menu
-<li class="vertical-menu__item">
-  <span class="vertical-menu__title">
-    <a href="https://google.com?search=how realisitic can you be" class="vertical-menu__link-title">
-    <span class="vertical-menu__link-text">Item #1</span>
-    </a>
-  </span>
-</li>
--->
-  */
 
   return mainMenuWrapper;
 }
