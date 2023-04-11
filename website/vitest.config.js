4bc01a7ac080ae3f06b1790e1e6de0fa31f8ca3e
Austin Haws <ahaws@utah.gov>
2023-04-11 12:25:57 -0600

    Discover tests needed for Website
    UDS-247 UDS-594

--- website/vitest.config.js
+++ website/vitest.config.js
@@ -1,19 +1,29 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   test: {
+    coverage: {
+      all: true,
+      exclude: [
+        '**/App.jsx',
+        '**/main.jsx',
+        '**/components/demo',
+        '**/HomeLanding.jsx',
+        '**/PLACEHOLDERDOCUMENTATION.jsx',
+        '**/FoundationLanding.jsx',
+        '**/GuidelinesLanding.jsx',
+        '**/LibraryLanding.jsx',
+        '**/websiteContent/resources',
+        '**/enums',
+        '**/colors.js',
+      ],
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'lcov'],
+    },
     globals: true,
   },
-  compilerOptions: {
-    types: ['vitest/globals'],
-  },
-  coverage: {
-    all: true,
-  },
-  environment: 'happy-dom',
   plugins: [
     eslintPlugin(),
   ],
