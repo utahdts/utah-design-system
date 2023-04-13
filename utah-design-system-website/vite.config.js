3981016d3dd85b188329ed2c862e40704408027b
Austin Haws <ahaws@utah.gov>
2023-04-13 13:15:48 -0600

    Update config for more scenarios Move folder structure to match workspaces
    UTAHDS-421

---
+++ utah-design-system-website/vite.config.js
@@ -0,0 +1,46 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+import mkcert from 'vite-plugin-mkcert';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  // throw new Error(`website mode: '${mode}'`);
+  return {
+    css: {
+      devSourcemap: true,
+    },
+    build: {
+      minify: false,
+      sourcemap: "inline",
+    },
+    resolve: {
+      // mode is `development`, `production`, and this allows a custom condition (otherwise it defaulted back to development)
+      conditions: [mode]
+    },
+    plugins: [
+      react(),
+      eslintPlugin(),
+      mkcert({
+        force: true,
+        hosts: ['*.utah.gov', '*.local.utah.gov', '127.0.0.1', 'localhost', '::1'],
+      }),
+    ],
+    server: {
+      fs: {
+        allow: [
+          // search up for workspace root
+          searchForWorkspaceRoot(process.cwd()),
+          // your custom rules
+          '../library'
+        ]
+      },
+      https: true,
+      host: 'designsystem.local.utah.gov',
+      port: 9180,
+    },
+    preview: {
+      port: 8080,
+    }
+  };
+});
