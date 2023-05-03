c538aeaf8d4fcdfde9517d5d8f5dc9fa7a67468e
Austin Haws <ahaws@utah.gov>
2023-05-03 10:30:49 -0600

    Add documentation for Switch
    UDS-409

--- utah-design-system-website/src/react/components/websiteContent/library/components/forms/Switch/SwitchDocumentation.jsx
+++ utah-design-system-website/src/react/components/websiteContent/library/components/forms/Switch/SwitchDocumentation.jsx
@@ -1,16 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable max-len */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable react/jsx-one-expression-per-line */
 import {
   Icons,
+  Switch,
+  Tab,
   TabGroup,
   TabList,
-  Tab,
+  TabPanel,
   TabPanels,
-  TabPanel, TableWrapper, Table, TableHead, TableHeadRow, TableHeadCell, TableBody, TableRow, TableCell, Switch, formElementSizesEnum,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableHeadRow,
+  TableRow,
+  TableWrapper,
+  formElementSizesEnum,
 } from '@utahdts/utah-design-system';
+import { Link } from 'react-router-dom';
+import pageUrls from '../../../../../routing/pageUrls';
 import SandboxExample from '../../../../../sandbox/SandboxExample';
-import SwitchExampleCodeReact from './SwitchExampleCodeReact';
-import SwitchExampleRender from './SwitchExampleRender';
-import SwitchPrimaryExampleProps from './SwitchExampleProps';
 import StaticExample from '../../../../../staticExamples/StaticExample';
+import SwitchExampleCodeReact from './SwitchExampleCodeReact';
+import SwitchPrimaryExampleProps from './SwitchExampleProps';
+import SwitchExampleRender from './SwitchExampleRender';
 
 /* eslint-disable react/jsx-one-expression-per-line */
 const propTypes = {};
@@ -20,7 +37,10 @@ function SwitchDocumentation() {
   return (
     <div className="documentation-content">
       <h1>Switch</h1>
-      <p className="lead-in">A <code>&lt;switch&gt;</code> element is a switchable component that switches on a switch action.</p>
+      <p className="lead-in">
+        Switches toggle the state of a single setting on or off. A switch must always be accompanied by a label, and
+        follows the same keyboard workflow as a <Link to={pageUrls.checkbox}>checkbox</Link>.
+      </p>
       <hr />
       <h2 id="example">Example</h2>
       <SandboxExample
@@ -94,7 +114,7 @@ function SwitchDocumentation() {
         )}
         quickTips={(
           <ul>
-            <li>An icon can be use within the indicator.</li>
+            <li>An <Link to={pageUrls.icons}>icon</Link> can be use within the indicator.</li>
           </ul>
         )}
       />
@@ -128,14 +148,99 @@ function SwitchDocumentation() {
         )}
       />
       <h2 className="mb-spacing" id="guidance">Guidance</h2>
-      <h3>When to use</h3>
+      <h3 id="section-when-to-use">When to use</h3>
       <ul className="mb-spacing">
-        <li>Use when a user is only allowed to select between two short options.</li>
-        <li>Preferred for short questions: <code>Yes/No</code> - <code>True/False</code>.</li>
+        <li><strong>Turning something on or off.</strong> Use to turn an option on/off, enable/disable something, or set a yes/no condition.</li>
       </ul>
-      <h3>When not to use</h3>
+
+      <h3 id="section-when-to-use-something-else">When to use something else</h3>
       <ul className="mb-spacing">
-        <li>Don&apos;t use for longer choice names.</li>
+        <li>
+          <strong>Selecting one item from a list.</strong> Consider using <Link to={pageUrls.radioButton}>radio buttons</Link> if users
+          need to select one item from a list of options.
+        </li>
+        <li>
+          <strong>Confirming an agreement.</strong> Consider using a <Link to={pageUrls.checkbox}>checkbox</Link> if users need to
+          confirm a statement such as an agreement.
+        </li>
+        <li>
+          <strong>Selecting multiple options.</strong> Consider using <Link to={pageUrls.checkbox}>checkboxes</Link> or
+          a <Link to={pageUrls.multiSelect}>multi-select</Link> if users can select multiple items from a list of options.
+        </li>
+      </ul>
+
+      <h3 id="section-usability">Usability guidance</h3>
+      <ul className="mb-spacing">
+        <li>
+          <strong>Enable or Disable:</strong> A switch is used to enable or disable a mode, feature, or function.
+        </li>
+        <li>
+          <strong>Label the behavior of the switch (state-label):</strong> Consider using a state-label inside the
+          switch such as &quot;on&quot; and &quot;off&quot; or &quot;enable&quot; and &quot;disable&quot; to communicate
+          the state of the switch.
+        </li>
+        <li>
+          <strong>Switches have an immediate effect on the system.</strong>
+        </li>
+        <li>
+          <strong>A switch has two states:</strong> Switches only have 2 states: on/enabled or off/disabled.
+        </li>
+        <li>
+          <strong>Form-label position:</strong> In a web form, the form-label is generally positioned to the right of the
+          switch, like the <Link to={pageUrls.checkbox}>checkbox</Link>.
+        </li>
+        <li>
+          <strong>Switch sizing:</strong> When pairing switches and <Link to={pageUrls.forms}>forms</Link>, use the same size.
+        </li>
+        <li>
+          <strong>Dynamic form-labels:</strong> When possible, avoid the use of dynamic form-labels. Instead, use the state-label
+          and switch color to convey its state (on or off).
+        </li>
+      </ul>
+
+      <h3 id="section-accessibility" className="mb-spacing">Accessibility</h3>
+      <h4 id="section-contrast">Contrast</h4>
+      <ul className="mb-spacing">
+        <li>Text within a switch must maintain a <code>4.5:1</code> contrast ratio.</li>
+        <li>The switch&apos;s boundaries must maintain a <code>3:1</code> contrast ratio against the background.</li>
+        <li>The switch focus state should be a <code>3:1</code> contrast ratio.</li>
+      </ul>
+
+      <h4 id="section-keyboard-interactivity">Keyboard interactivity</h4>
+      <ul className="mb-spacing">
+        <li>Use the <code>tab</code> key to move between form elements. The switch should display a visible focus state when users tab to it.</li>
+        <li>A switch component can be triggered by pressing the <code>spacebar</code>.</li>
+      </ul>
+
+      <h4 id="section-screen-readers">Screen readers</h4>
+      <ul className="mb-spacing">
+        <li>
+          The state of the switch (on/off, enabled/disabled) must be conveyed audibly to a screen reader user.
+        </li>
+        <li>
+          The switch provided by the Utah Design System is a restyled <Link to={pageUrls.checkbox}>checkbox</Link>. Where possible,
+          it is recommended that you use native elements rather than using aria and role attributes, as native elements are more
+          widely supported by user agents and assistive technology. Remember: The first rule of ARIA: Before you use ARIA, use
+          native HTML elements or attributes first!
+        </li>
+        <li>
+          All form controls should have labels, including <Link to={pageUrls.radioButton}>radio buttons</Link>, <Link to={pageUrls.checkbox}>checkboxes</Link>, and
+          switches. In most cases, this is done by using the <code>&lt;label&gt;</code> element.
+        </li>
+        <li>
+          When a label can&apos;t be used, it&apos;s necessary to add an attribute directly to the input component. In this case,
+          you can apply the additional attribute (e.g. <code>aria-label</code>, <code>aria-labelledby</code>, or <code>visually-hidden</code>).
+        </li>
+        <li>
+          By default, switches include a <code>role=&quot;switch&quot;</code> ARIA attribute to ensure they are accurately announced
+          via assistive technologies.
+        </li>
+        <li>
+          By default, switch groups include a <code>role=&quot;group&quot;</code> on the <code>&lt;fieldset&gt;</code> wrapping grouped switches.
+        </li>
+        <li>
+          If you customize this component, ensure that it continues to meet the accessibility requirements.
+        </li>
       </ul>
 
       <h2 id="settings">Settings and Props</h2>
