c829df6e9ba6b93d0c26398e667ef436627aa804
Joseph Sharp <jmsharp@utah.gov>
2023-07-10 10:13:48 -0600

    Review elevation

--- utah-design-system-website/src/react/components/websiteContent/guidelines/DepthElevationShadowsDocumentation.jsx
+++ utah-design-system-website/src/react/components/websiteContent/guidelines/DepthElevationShadowsDocumentation.jsx
@@ -5,7 +5,8 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import { Link } from 'react-router-dom';
 import pageUrls from '../../routing/pageUrls';
-import StaticExample from '../../staticExamples/StaticExample';
+import PreCodeForCodeString from '../../preCode/PreCodeForCodeString';
+import elevationExample from '../../../../static/images/screenshots/spacing/guidelines/elevation-best-practice.webp';
 
 const propTypes = {};
 const defaultProps = {};
@@ -13,7 +14,7 @@ const defaultProps = {};
 function DepthElevationShadowsDocumentation() {
   return (
     <div className="documentation-content">
-      <h1 id="h1-top">Depth Elevation & Shadows</h1>
+      <h1 id="h1-top">Depth, Elevation, & Shadows</h1>
       <p className="lead-in">
         At its core, elevation deals with the different layers of components on a website. The terms &quot;depth&quot; and &quot;shadows&quot; are
         also used interchangeably. However, they refer to the underlying structure of how components are positioned on a site. By combining
@@ -25,32 +26,41 @@ function DepthElevationShadowsDocumentation() {
         options include a border to assist in low contrast situations and three without borders when the contrast is high enough on its own. Below are
         the elevation specifications provided with the design system:
       </p>
+      <hr />
 
-      <StaticExample
-        renderedExample="Example coming soon!"
-        quickTips={(
-          <>
-            <p>Border</p>
-            <ul>
-              <li>All borders are: <code>1px solid #474747</code></li>
-              <li>Small <code>X - 0, Y - 3,  B - 6, rgba(0, 0, 0, 0.3)</code></li>
-              <li>Medium <code>X - 0, Y - 6,  B - 12, rgba(0, 0, 0, 0.3)</code></li>
-              <li>Large <code>X - 0, Y - 12,  B - 16, rgba(0, 0, 0, 0.3)</code></li>
-            </ul>
-            <p>Borderless</p>
-            <ul>
-              <li>Small <code>X - 0, Y - 0,  B - 8, rgba(0, 0, 0, 0.4)</code></li>
-              <li>Medium <code>X - 0, Y - 3,  B - 14, rgba(0, 0, 0, 0.45)</code></li>
-              <li>Large <code>X - 0, Y - 6,  B - 20, rgba(0, 0, 0, 0.5)</code></li>
-            </ul>
-          </>
-        )}
+      <h2 id="section-example">Examples</h2>
+
+      <div className="flex justify-center gap-l flex-wrap my-spacing-l">
+        <div className="depth-example depth-example__small depth-example--border">Example<br />Small<br />Border</div>
+        <div className="depth-example depth-example__small-borderless">Example<br />Small<br />Borderless</div>
+        <div className="depth-example depth-example__medium depth-example--border">Example<br />Medium<br />Border</div>
+        <div className="depth-example depth-example__medium-borderless">Example<br />Medium<br />Borderless</div>
+        <div className="depth-example depth-example__large depth-example--border">Example<br />Large<br />Border</div>
+        <div className="depth-example depth-example__large-borderless">Example<br />Large<br />Borderless</div>
+      </div>
+
+      <h3 id="section-example">CSS Variables for Drop Shadows</h3>
+      <PreCodeForCodeString
+        showBackgroundColor
+        allowScrollOverflow
+        maxHeight="300px"
+        codeRaw={`
+          /* elevation box shadows */
+          --drop-shadow-color: rgba(0, 0, 0, 0.3);
+          --drop-shadow-color-top: rgba(0, 0, 0, 0.15);
+          --elevation-small: 0 3px 6px var(--drop-shadow-color);
+          --elevation-medium: 0 6px 12px var(--drop-shadow-color);
+          --elevation-large: 0 12px 16px var(--drop-shadow-color);
+          --elevation-small-borderless: 0 3px 6px var(--drop-shadow-color), 0 -3px 3px var(--drop-shadow-color-top);
+          --elevation-medium-borderless: 0 6px 12px var(--drop-shadow-color), 0 -3px 6px var(--drop-shadow-color-top);
+          --elevation-large-borderless: 0 12px 16px var(--drop-shadow-color),  0 -3px 12px var(--drop-shadow-color-top);
+        `}
       />
 
       <hr />
 
       <h2 id="section-areas-to-consider" className="mb-spacing">Areas to Consider</h2>
-      <h3 id="section-accessibility" className="mb-spacing">Accessibility</h3>
+      <h3 id="section-accessibility">Accessibility</h3>
       <p>
         It&apos;s important to note that there are no specific, universally mandated accessibility requirements dedicated to shadows. Instead,
         the broader principles of contrast and clarity can indirectly influence how shadows are implemented. Pay special attention to contrast,
@@ -60,25 +70,25 @@ function DepthElevationShadowsDocumentation() {
         For more information please refer to the <Link to={pageUrls.accessibility}>accessibility guidance</Link>.
       </p>
 
-      <h3 id="section-layering-stacking" className="mb-spacing">Layering and Stacking</h3>
-      <p>
+      <h3 id="section-layering-stacking">Layering and Stacking</h3>
+      <p className="mb-auto">
         Utilize different elevations to create a sense of depth and order. For example:
       </p>
       <ul>
-        <li><strong>Multi surface effect.</strong> The perception can be created where it looks as if content is moving underneath a menu bar.</li>
-        <li><strong>Minimal focus point.</strong> Toggling a <Link to={pageUrls.tooltips}>tooltip</Link>, where it hovers over the primary content.</li>
+        <li>
+          <strong>Multi surface effect.</strong> The perception of multiple layers can be created by positioning elements on top of other elements.
+          Box shadows of different distances on the <code>x</code> and <code>y</code> axis can help contribute to this sense of elevation.
+        </li>
+        <li><strong>Focus attention.</strong> Toggling a <Link to={pageUrls.tooltips}>tooltip</Link>, when a user hovers over an element to provide additional clarity.</li>
       </ul>
-      <p>
+      <p className="mt-spacing">
         To maintain a clean and focused user interface, it is advisable to refrain from overusing shadows. Excessive shadow effects can introduce
         visual clutter and draw users&apos; attention away from the intended focal points. When shadows are used sparingly and purposefully, they
         can enhance the hierarchy and emphasize important elements. However, exercising restraint in their application is key to preventing distractions
         and ensuring a seamless user experience.
       </p>
 
-      <StaticExample
-        renderedExample="Example coming soon?"
-        quickTips="Are you as confused with the interface below as we are?"
-      />
+      <img src={elevationExample} alt="Example of acceptable and unacceptable elevation usage" style={{ maxWidth: '100%' }} />
 
       <h3 id="section-direction-guidance" className="mb-spacing">Direction & Guidance</h3>
       <ul>
@@ -88,7 +98,8 @@ function DepthElevationShadowsDocumentation() {
         </li>
         <li>
           <strong>Subtle shadows.</strong> Shadows should be subtle and natural-looking. Avoid using harsh or unrealistic shadows that may distract
-          or confuse users. Employ shadows with low opacity and feathered edges. Consider the light source to create a realistic effect.
+          or confuse users. Employ shadows with low opacity and feathered edges. Consider the light source to create a realistic effect (typically the light source would
+          originate from the top of the viewport).
         </li>
         <li>
           <strong>Focus indication.</strong> When an elevated component is interactive or focusable, it&apos;s crucial to provide a clear and visible indication
@@ -98,12 +109,12 @@ function DepthElevationShadowsDocumentation() {
         <li>
           <strong>Text legibility.</strong> If text is placed on an elevated surface, ensure that it remains legible and easy to read. Consider the color contrast
           between the text and its background, as well as the size and type of font used. The text should have sufficient contrast and be large enough to be read
-          comfortably by users with visual impairments.
+          comfortably by users with visual impairments. See <Link to={pageUrls.accessibility}>accessibility</Link>.
         </li>
         <li>
-          <strong>Motion sensitivity.</strong> Some users may be sensitive to motion or have vestibular disorders. When using elevation effects or animations, ensure
-          they are subtle and do not cause excessive motion that may be uncomfortable or disorienting for certain individuals. Provide options to disable or customize
-          animations, if possible.
+          <strong>Motion sensitivity.</strong> Some users may be sensitive to motion or have vestibular disorders. When using motion or animations, ensure
+          they are subtle and not excessive. Motion may be uncomfortable or disorienting for certain individuals. Provide options to disable or customize
+          animations, if possible. See <Link to={pageUrls.accessibility}>accessibility</Link>.
         </li>
         <li>
           <strong>Consider depth cues.</strong> Use visual cues such as shadows and gradients to indicate depth and separation between components. These cues help
