6dc1949514948dbfc98d7eff4e4d5dbd4b818b9c
Austin Haws <ahaws@utah.gov>
2023-08-02 12:24:06 -0600

    Fix errors from merge
    :profanity::profanity::profanity::profanity:

--- utah-design-system-website/scripts/submitSitemap.js
+++ utah-design-system-website/scripts/submitSitemap.js
@@ -1,1 +1,10 @@
-console.log('sitemap submission not yet implemented');
+import { exec } from 'child_process';
+
+// https://www.google.com/ping?sitemap=https://example.com/sitemap.xml
+
+exec(
+  'ping -c 3 https://designsystem.utah.gov/sitemap.txt',
+  (_err, stdout) => {
+    console.log(stdout);
+  }
+);
