d8ec806b30160f7c27ae9e97ef6174cb0a11ace9
sfarnsworth <sfarnsworth@utah.gov>
2023-08-24 19:42:25 -0600

    Replacing atomic png for svg

--- @utahdts/utah-design-system/react/hooks/useBanner.js
+++ @utahdts/utah-design-system/react/hooks/useBanner.js
@@ -1,3 +1,4 @@
+/* eslint no-console: ["error", { allow: ["log"] }] */
 export default function useBanner() {
   return ({ message }) => {
     // TODO: replace this console.log with adding the banner to the banner queue in the design system context
