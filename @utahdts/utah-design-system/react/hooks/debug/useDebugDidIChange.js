106ce7198e2d996712af0108d56a824db0843596
Austin Haws <ahaws@utah.gov>
2023-08-31 07:48:16 -0600

    Add useDebugDidChange hook

---
+++ @utahdts/utah-design-system/react/hooks/debug/useDebugDidIChange.js
@@ -0,0 +1,18 @@
+// @ts-check
+import { useRef } from 'react';
+
+/**
+ * check if a value changes between renders
+ * helpful for debugging useEffect/useMemo/useCallback etc
+ * @template FieldT
+ * @param {FieldT} field
+ * @param {string} fieldName
+ */
+export default function useDebugDidIChange(field, fieldName) {
+  const fieldRef = useRef(field);
+  if (fieldRef.current !== field) {
+    // eslint-disable-next-line no-console
+    console.log(`CHANGED: ${fieldName}; '${fieldRef.current}' => '${field}`);
+    fieldRef.current = field;
+  }
+}
