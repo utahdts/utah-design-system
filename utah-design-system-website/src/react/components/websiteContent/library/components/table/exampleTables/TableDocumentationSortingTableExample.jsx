43cdc8e4de218a420db1f30e0543f63c94a65848
Gaetan Grimaud <ggrimaud@utah.gov>
2023-09-01 13:48:48 -0600

    UDS-335: Put table examples in accordions

--- utah-design-system-website/src/react/components/websiteContent/library/components/table/exampleTables/TableDocumentationSortingTableExample.jsx
+++ utah-design-system-website/src/react/components/websiteContent/library/components/table/exampleTables/TableDocumentationSortingTableExample.jsx
@@ -1,4 +1,5 @@
 import {
+  Accordion,
   Button,
   Table,
   TableBody,
@@ -28,64 +29,66 @@ function TableDocumentationSortingTableExample() {
         id="table__sorting-table-example"
         linkUrl="https://github.com/utahdts/utah-design-system/tree/main/utah-design-system-website/src/react/components/websiteContent/library/components/table/exampleTables/TableDocumentationSortingTableExample.jsx"
       />
-      <TableWrapper>
-        <Table className="table table--lines-x table--alt table--v-align-center table--condensed">
-          <TableHead>
-            <TableSortingRules defaultValue="name">
-              {/*
-                Order here determines which rules to apply first.
-                The "selected" TableHeadCell's "recordFieldPath" is pushed to the top of the TableSortingRule list
-                and then the rest of the TableSortingRules are applied in order.
-              */}
-              <TableSortingRule
-                recordFieldPath="name"
-                // sort by lastName
-                customSort={({ fieldValueA, fieldValueB }) => {
-                  function getLastName(fullName) {
-                    return (fullName || '').split(' ').pop();
-                  }
-                  return getLastName(fieldValueA).localeCompare(getLastName(fieldValueB));
-                }}
-              />
-              <TableSortingRule recordFieldPath="nthPresident" fieldType={tableSortingRuleFieldType.NUMBER} defaultIsAscending={false} />
-              <TableSortingRule recordFieldPath="politicalParty" />
-              <TableSortingRule recordFieldPath="inauguration" />
-              <TableSortingRule recordFieldPath="inaugurationDate" fieldType={tableSortingRuleFieldType.DATE} />
-              <TableSortingRule recordFieldPath="birthplace.state" />
-              <TableSortingRule recordFieldPath="birthplace.county" />
-            </TableSortingRules>
+      <Accordion headerContent={<span>Table Preview</span>} headerClassName="button--primary-color button--solid" headingLevel={4}>
+        <TableWrapper>
+          <Table className="table table--lines-x table--alt table--v-align-center table--condensed">
+            <TableHead>
+              <TableSortingRules defaultValue="name">
+                {/*
+                  Order here determines which rules to apply first.
+                  The "selected" TableHeadCell's "recordFieldPath" is pushed to the top of the TableSortingRule list
+                  and then the rest of the TableSortingRules are applied in order.
+                */}
+                <TableSortingRule
+                  recordFieldPath="name"
+                  // sort by lastName
+                  customSort={({ fieldValueA, fieldValueB }) => {
+                    function getLastName(fullName) {
+                      return (fullName || '').split(' ').pop();
+                    }
+                    return getLastName(fieldValueA).localeCompare(getLastName(fieldValueB));
+                  }}
+                />
+                <TableSortingRule recordFieldPath="nthPresident" fieldType={tableSortingRuleFieldType.NUMBER} defaultIsAscending={false} />
+                <TableSortingRule recordFieldPath="politicalParty" />
+                <TableSortingRule recordFieldPath="inauguration" />
+                <TableSortingRule recordFieldPath="inaugurationDate" fieldType={tableSortingRuleFieldType.DATE} />
+                <TableSortingRule recordFieldPath="birthplace.state" />
+                <TableSortingRule recordFieldPath="birthplace.county" />
+              </TableSortingRules>
 
-            <TableHeadRow>
-              <TableHeadCell recordFieldPath="name" className="text-left">Name</TableHeadCell>
-              <TableHeadCell recordFieldPath="nthPresident" className="text-left">No.</TableHeadCell>
-              <TableHeadCell recordFieldPath="politicalParty" className="text-left">Party</TableHeadCell>
-              <TableHeadCell recordFieldPath="inauguration" className="text-left">Inauguration</TableHeadCell>
-              {/* <TableHeadCell recordFieldPath="funFacts" className="text-left">Fun Facts</TableHeadCell> */}
-              <TableHeadCell recordFieldPath="birthplace" tableSortingFieldPaths={['birthplace.state', 'birthplace.county']} className="text-left">Birth Place</TableHeadCell>
-              <TableHeadCell />
-            </TableHeadRow>
-          </TableHead>
+              <TableHeadRow>
+                <TableHeadCell recordFieldPath="name" className="text-left">Name</TableHeadCell>
+                <TableHeadCell recordFieldPath="nthPresident" className="text-left">No.</TableHeadCell>
+                <TableHeadCell recordFieldPath="politicalParty" className="text-left">Party</TableHeadCell>
+                <TableHeadCell recordFieldPath="inauguration" className="text-left">Inauguration</TableHeadCell>
+                {/* <TableHeadCell recordFieldPath="funFacts" className="text-left">Fun Facts</TableHeadCell> */}
+                <TableHeadCell recordFieldPath="birthplace" tableSortingFieldPaths={['birthplace.state', 'birthplace.county']} className="text-left">Birth Place</TableHeadCell>
+                <TableHeadCell />
+              </TableHeadRow>
+            </TableHead>
 
-          <TableBody>
-            <TableBodyData records={examplePresidentsData} recordIdField="id">
-              <TableBodyDataRowTemplate>
-                <TableBodyDataCellTemplate recordFieldPath="name" />
-                <TableBodyDataCellTemplate recordFieldPath="nthPresident" />
-                <TableBodyDataCellTemplate recordFieldPath="politicalParty" />
-                <TableBodyDataCellTemplate recordFieldPath="inauguration" />
-                {/* <TableBodyDataCellTemplate recordFieldPath="funFacts" /> */}
-                <TableBodyDataCellTemplate recordFieldPath="birthPlace">
-                  {({ record }) => [record.birthplace.county, record.birthplace.state].join(', ')}
-                </TableBodyDataCellTemplate>
-                <TableBodyDataCellTemplate>
-                  <Button className="button--small" onClick={() => { }}>More</Button>
-                </TableBodyDataCellTemplate>
-              </TableBodyDataRowTemplate>
-            </TableBodyData>
-          </TableBody>
+            <TableBody>
+              <TableBodyData records={examplePresidentsData} recordIdField="id">
+                <TableBodyDataRowTemplate>
+                  <TableBodyDataCellTemplate recordFieldPath="name" />
+                  <TableBodyDataCellTemplate recordFieldPath="nthPresident" />
+                  <TableBodyDataCellTemplate recordFieldPath="politicalParty" />
+                  <TableBodyDataCellTemplate recordFieldPath="inauguration" />
+                  {/* <TableBodyDataCellTemplate recordFieldPath="funFacts" /> */}
+                  <TableBodyDataCellTemplate recordFieldPath="birthPlace">
+                    {({ record }) => [record.birthplace.county, record.birthplace.state].join(', ')}
+                  </TableBodyDataCellTemplate>
+                  <TableBodyDataCellTemplate>
+                    <Button className="button--small" onClick={() => { }}>More</Button>
+                  </TableBodyDataCellTemplate>
+                </TableBodyDataRowTemplate>
+              </TableBodyData>
+            </TableBody>
 
-        </Table>
-      </TableWrapper>
+          </Table>
+        </TableWrapper>
+      </Accordion>
     </div>
   );
 }
