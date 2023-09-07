328dd72d184ca03e3b080f1e9f56a7d788b07b62
Gaetan Grimaud <ggrimaud@utah.gov>
2023-09-07 11:12:21 -0600

    UDS-1183: Update documentation for isClearable

--- utah-design-system-website/src/react/components/websiteContent/library/components/forms/TextInput/TextInputCssClassesDocumentation.jsx
+++ utah-design-system-website/src/react/components/websiteContent/library/components/forms/TextInput/TextInputCssClassesDocumentation.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import {
+  Table, TableBody, TableCell, TableHead, TableHeadCell, TableHeadRow, TableRow, TableWrapper
+} from '@utahdts/utah-design-system';
 
 function TextInputCssClassesDocumentation() {
   return (
-    <div>
-      No specific class.
-    </div>
+    <TableWrapper>
+      <Table className="table--lines-x">
+        <TableHead>
+          <TableHeadRow>
+            <TableHeadCell className="text-left css-classes">CSS Classes</TableHeadCell>
+            <TableHeadCell className="text-left ">Description</TableHeadCell>
+          </TableHeadRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell><code>.text-input--clear-icon-visible</code></TableCell>
+            <TableCell>This class is used conjointly with the <code>isClearable</code> property.</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableWrapper>
   );
 }
 
