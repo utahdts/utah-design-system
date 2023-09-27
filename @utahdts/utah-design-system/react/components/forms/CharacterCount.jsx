8450a40a1dd9ec2d2475f037c1d81f71f2b24504
Gaetan Grimaud <ggrimaud@utah.gov>
2023-09-27 16:39:39 -0600

    CharacterCount - aria-live

--- @utahdts/utah-design-system/react/components/forms/CharacterCount.jsx
+++ @utahdts/utah-design-system/react/components/forms/CharacterCount.jsx
@@ -1,4 +1,7 @@
 import PropTypes from 'prop-types';
+import {
+  useCallback, useEffect, useRef, useState
+} from 'react';
 import joinClassNames from '../../util/joinClassNames';
 
 const propTypes = {
@@ -19,18 +22,35 @@ const defaultProps = {
  * @returns {JSX.Element}
  */
 export default function CharacterCount({ maxLength, id, value }) {
+  const [ariaMessage, setAriaMessage] = useState(undefined);
+  const timer = useRef(undefined);
+
   const charactersLeft = value?.length ? (maxLength - value.length) : maxLength;
   const overLimit = value?.length && (value.length > maxLength);
   const charactersOver = value?.length ? (value.length - maxLength) : 0;
+
+  const displayMessage = useCallback(() => (
+    overLimit
+      ? `${charactersOver} character${(charactersOver >= 2 ? 's' : '')} over the limit.`
+      : `${charactersLeft} character${(charactersLeft >= 2 ? 's' : '')} left.`
+  ), [charactersLeft, charactersOver, overLimit]);
+
+  useEffect(() => {
+    clearTimeout(timer.current);
+    timer.current = setTimeout(() => {
+      setAriaMessage(displayMessage());
+      timer.current = undefined;
+    }, 1500);
+  }, [value]);
+
   return (
     maxLength
       ? (
-        <div className={joinClassNames('input-wrapper__character-count', overLimit && 'over-limit')} id={`${id}-character-count`}>
-          {
-            overLimit
-              ? `${charactersOver} character${(charactersOver >= 2 ? 's' : '')} over the limit.`
-              : `${charactersLeft} character${(charactersLeft >= 2 ? 's' : '')} left.`
-          }
+        <div>
+          <div className={joinClassNames('input-wrapper__character-count', overLimit && 'over-limit')} id={`${id}-character-count`}>
+            {displayMessage()}
+          </div>
+          <div className="visually-hidden" aria-live="polite">{ariaMessage}</div>
         </div>
       )
       : null
