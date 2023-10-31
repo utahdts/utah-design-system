7353036dd424fea9dc0d9bce1a68043ae069fdfa
Austin Haws <ahaws@utah.gov>
2023-10-31 14:51:57 -0600

    Apply Sonar updates
    UDS-1218

--- utah-design-system-website/src/react/components/websiteContent/Page404.jsx
+++ utah-design-system-website/src/react/components/websiteContent/Page404.jsx
@@ -27,7 +27,7 @@ function Page404() {
           </p>
           <p className="mb-spacing-l">
             <Link to={pageUrls.home} className="button button--primary-color button--solid" style={{ display: 'inline-flex' }}>{/*
-              */}<span className="button--icon button--icon-left"><span className="utds-icon-before-arrow-left" aria-hidden="true" style={{ fontSize: '.9rem' }} /></span>
+              */}<span className="button--icon button--icon-left"><span className="utds-icon-before-arrow-left" aria-hidden="true" style={{ fontSize: '.9rem' }} /></span>{' '}
               Home Page
             </Link>
           </p>
