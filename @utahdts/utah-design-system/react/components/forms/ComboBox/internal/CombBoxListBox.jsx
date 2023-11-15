eef26846165b6e654f27d39b4d21f9fd2448a959
Austin Haws <ahaws@utah.gov>
2023-11-15 10:46:42 -0700

    Pop combo box options
    UDS-1332

--- @utahdts/utah-design-system/react/components/forms/ComboBox/internal/CombBoxListBox.jsx
+++ @utahdts/utah-design-system/react/components/forms/ComboBox/internal/CombBoxListBox.jsx
@@ -1,5 +1,7 @@
 // @ts-check
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { usePopper } from 'react-popper';
+import popupPlacement from '../../../../enums/popupPlacement';
 import joinClassNames from '../../../../util/joinClassNames';
 import ComboBoxOption from '../ComboBoxOption';
 import useComboBoxContext from '../context/useComboBoxContext';
@@ -10,6 +12,7 @@ import useComboBoxContext from '../context/useComboBoxContext';
  * @param {Object} props
  * @param {string} props.ariaLabelledById
  * @param {React.ReactNode | null} [props.children]
+ * @param {React.MutableRefObject<any>} props.popperReferenceElementRef
  * @param {string} props.id
  * @returns {JSX.Element}
  */
@@ -17,22 +20,40 @@ export default function CombBoxListBox({
   ariaLabelledById,
   children,
   id,
+  popperReferenceElementRef,
 }) {
   const [{ isOptionsExpanded }] = useComboBoxContext();
   const [{ optionsFiltered }] = useComboBoxContext();
   const ulRef = useRef(/** @type {HTMLUListElement | null} */(null));
 
+  const { styles, attributes, update } = usePopper(
+    popperReferenceElementRef.current,
+    ulRef.current,
+    { placement: popupPlacement.BOTTOM }
+  );
+
+  useEffect(
+    () => {
+      if (update) {
+        update();
+      }
+    },
+    [isOptionsExpanded, update]
+  );
+
   return (
     <ul
       id={id}
+      aria-labelledby={ariaLabelledById}
       className={joinClassNames(
         'combo-box__list-box',
         !isOptionsExpanded && 'visually-hidden'
       )}
-      role="listbox"
-      aria-labelledby={ariaLabelledById}
-      tabIndex={-1}
       ref={ulRef}
+      role="listbox"
+      style={styles.popper}
+      tabIndex={-1}
+      {...attributes.popper}
     >
       {children}
       {optionsFiltered?.length ? null : <ComboBoxOption isStatic isDisabled label="" value="">No results found</ComboBoxOption>}
