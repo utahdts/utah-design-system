93732acf989826460879cb8a25296f49464264a8
Gaetan Grimaud <ggrimaud@utah.gov>
2024-01-12 09:03:54 -0700

    UDS-328: File input tweaks

--- @utahdts/utah-design-system/react/components/forms/FileInput.jsx
+++ @utahdts/utah-design-system/react/components/forms/FileInput.jsx
@@ -4,25 +4,31 @@ import { RequiredStar } from './RequiredStar';
 import { joinClassNames } from '../../util/joinClassNames';
 import { Tag } from '../buttons/Tag';
 import { useAriaMessaging } from '../../contexts/UtahDesignSystemContext/hooks/useAriaMessaging';
+import { ErrorMessage } from './ErrorMessage';
 
 /**
  * @param {object} props
  * @param {string} [props.accept]
  * @param {string} [props.className]
+ * @param {string} [props.errorMessage]
  * @param {string} [props.hint]
  * @param {string} props.id
+ * @param {import('react').Ref<HTMLDivElement>} [props.innerRef]
  * @param {string} props.label
  * @param {string} [props.name]
+ * @param {boolean} [props.isDisabled]
  * @param {boolean} [props.isRequired]
  * @param {import('react').ChangeEventHandler} [props.onChange]
  * @returns {import('react').JSX.Element}
- * @class
  */
 export function FileInput({
   accept,
   className,
+  errorMessage,
   hint,
   id,
+  innerRef,
+  isDisabled,
   isRequired,
   label,
   name,
@@ -30,48 +36,58 @@ export function FileInput({
 }) {
   const { addPoliteMessage } = useAriaMessaging();
   const [isDragged, setDragged] = useImmer(false);
-  const [files, setFiles] = useImmer([]);
+  const [file, setFile] = useImmer('');
 
   const currentOnChange = useCallback((/** @type {import("react").ChangeEvent<Element>} */ event) => {
     // @ts-ignore
-    const filesList = [...event.target.files].map((file) => file?.name);
-    // @ts-ignore
-    setFiles(filesList);
+    setFile([...event.target.files][0]?.name || '');
     if (onChange) {
       onChange(event);
     }
   }, []);
 
   useEffect(() => {
-    if (files.length) {
-      addPoliteMessage(`You have selected the file: ${files[0]}.`);
+    if (file.length) {
+      addPoliteMessage(`You have selected the file: ${file}.`);
     } else {
       addPoliteMessage('No file selected.');
     }
-  }, [files]);
+  }, [file]);
+
   return (
-    <div className="input-wrapper">
+    <div className="input-wrapper" ref={innerRef}>
       <label htmlFor={id}>
         {label}
         {isRequired ? <RequiredStar /> : null}
       </label>
       {hint
         ? (
-          <div className="info-box">
-            <div className="info-box__content">{hint}</div>
+          <div className="info-box file-input__info-box mt-spacing-xs">
+            <div className="info-box__content">
+              {hint}
+            </div>
           </div>
         )
         : null}
-      <div className={joinClassNames('file-input__box mt-spacing-s', isDragged ? 'file-input__box--dragged' : '')}>
-        {files.length
+      <div className={joinClassNames(
+        'file-input__box mt-spacing-s',
+        isDragged ? 'file-input__box--dragged' : '',
+        isDisabled ? 'file-input__box--disabled' : ''
+      )}
+      >
+        {file.length
           ? (
             <div className="file-input__single-file">
               <div className="flex justify-between items-center">
-                <span className="font-bold mr-spacing">{files.length} file{files.length > 1 ? 's' : ''} selected</span>
+                <span className="font-bold mr-spacing">1 file selected</span>
                 <span className="button button--small">Change file</span>
               </div>
               <hr />
-              <div className="file-input__file-list flex-wrap">{files.map((file) => <Tag key={file}>{file}</Tag>)}</div>
+              <div className="file-input__file-list flex-wrap">
+                <Tag>
+                  {file}
+                </Tag>
+              </div>
             </div>
           )
           : (
@@ -81,16 +97,19 @@ export function FileInput({
           )}
         <input
           accept={accept}
+          aria-describedby={errorMessage ? `${id}-error` : undefined}
           className={className}
+          disabled={isDisabled}
           id={id}
           name={name || id}
-          type="file"
           onChange={currentOnChange}
-          onDrop={() => setDragged(false)}
           onDragEnter={() => setDragged(true)}
           onDragLeave={() => setDragged(false)}
+          onDrop={() => setDragged(false)}
+          type="file"
         />
       </div>
+      <ErrorMessage errorMessage={errorMessage} id={id} />
     </div>
   );
 }
