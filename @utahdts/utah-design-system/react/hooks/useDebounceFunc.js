0e40f751216e4fa06897921ca94f8a5e6df6fcc2
Austin Haws <ahaws@utah.gov>
2024-02-26 11:10:42 -0700

    Fix spread debounce parameters from being arrays

--- @utahdts/utah-design-system/react/hooks/useDebounceFunc.js
+++ @utahdts/utah-design-system/react/hooks/useDebounceFunc.js
@@ -1,6 +1,15 @@
 import { useCallback, useEffect, useRef } from 'react';
 
 /**
+ * !!!!! COMMON HOOK !!!!!
+ *
+ * When you change this hook, make sure to update it also in the stack-base repo:
+ * https://github.com/utahdts/dtt-js-stack-base/tree/main/_REACT_/hooks/useDebounceFunc.js
+ *
+ * !!!!! COMMON HOOK !!!!!
+ */
+
+/**
  * Given a function, return another function that will prevent the given function
  * from being executed until the "delay" time has expired. It will fire the function
  * immediately and then have a cool down period until it fires again.
@@ -17,23 +26,23 @@ export function useDebounceFunc(func, delay = 1000) {
   useEffect(() => () => clearTimeout(timeoutRef.current), []);
 
   return useCallback(
-    (...varArgs) => {
+    (param) => {
       const promise = new Promise((resolve) => {
         if (!lastInvocationRef.current || ((lastInvocationRef.current - (new Date()).getTime()) >= delay)) {
           clearTimeout(timeoutRef.current);
           lastInvocationRef.current = (new Date()).getTime();
           lastVarArgsRef.current = null;
-          func(...varArgs);
+          func(param);
         } else {
-          lastVarArgsRef.current = varArgs;
+          lastVarArgsRef.current = param;
           lastInvocationRef.current = (new Date()).getTime();
           clearTimeout(timeoutRef.current);
           timeoutRef.current = window.setTimeout(
             () => {
-              func(...(lastVarArgsRef.current || []));
+              func(lastVarArgsRef.current);
               lastVarArgsRef.current = null;
               lastInvocationRef.current = NaN;
-              resolve(varArgs);
+              resolve(param);
             },
             delay
           );
