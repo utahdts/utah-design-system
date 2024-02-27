62c7cdad153630c0bd71cc6cd66fcda656f4f6d2
Austin Haws <ahaws@utah.gov>
2024-02-27 16:49:22 -0700

    Add option `parentMenuLinkSuffix` for menus
    This option allows customizing the suffix for menus that are links
    and have children menu items that have an "Overview" link added to the
    top of the children.
    
    UDS-1494

--- @utahdts/utah-design-system-header/artifacts/index.d.ts
+++ @utahdts/utah-design-system-header/artifacts/index.d.ts
@@ -13,118 +13,14 @@
   Super painful to have to do this manual. But a better way is sure to surface at some point of time.
 */
 declare module "@utahdts/utah-design-system-header" {
-  export type ChildrenMenuTypes = ('flyout' | 'inline' | 'mega-menu' | 'plain');
-  export enum childrenMenuTypes {
-    FLYOUT = 'flyout',
-    INLINE = 'inline',
-    MEGA_MENU = 'mega-menu',
-    PLAIN = 'plain',
-  }
-
-  export type Events = 'utahHeaderLoaded' | 'utahHeaderUnloaded';
-  export enum events {
-    HEADER_LOADED = 'utahHeaderLoaded',
-    HEADER_UNLOADED = 'utahHeaderUnloaded',
-  }
-
-  export type PopupPlacement = 'auto' | 'auto-start' | 'auto-end' | 'bottom' | 'bottom-start' | 'bottom-end' | 'left' | 'left-start' | 'left-end' | 'right' | 'right-start' | 'right-end' | 'top' | 'top-start' | 'top-end';
-  export enum popupPlacement {
-    AUTO = 'auto',
-    AUTO_START = 'auto-start',
-    AUTO_END = 'auto-end',
-    BOTTOM = 'bottom',
-    BOTTOM_START = 'bottom-start',
-    BOTTOM_END = 'bottom-end',
-    LEFT = 'left',
-    LEFT_START = 'left-start',
-    LEFT_END = 'left-end',
-    RIGHT = 'right',
-    RIGHT_START = 'right-start',
-    RIGHT_END = 'right-end',
-    TOP = 'top',
-    TOP_START = 'top-start',
-    TOP_END = 'top-end',
-  }
-
-  export type Sizes = 'SMALL' | 'MEDIUM' | 'LARGE';
-  export enum sizes {
-    SMALL = 'SMALL',
-    MEDIUM = 'MEDIUM',
-    LARGE = 'LARGE',
-  }
-
-  export function renderDOMSingle(str: string | HTMLElement): HTMLElement;
-  export const defaultSettings: Settings;
-
-  export type DomLocationTarget = {
-    cssSelector?: string | undefined;
-    element?: HTMLElement | undefined;
-    elementFunction?: (() => HTMLElement) | undefined;
-  };
-
   export type EventAction = ((arg0: MouseEvent | TouchEvent | KeyboardEvent) => void);
-
-  export type MenuItemUrlAction = {
-    url: string;
-    openInNewTab?: boolean | undefined;
-  };
-
-  export type MenuItemFunctionUrlAction = {
-    actionFunction: EventAction;
-    skipHandleEvent?: boolean | undefined;
-    openInNewTab?: boolean | undefined;
-    url: string;
-  };
-
-  export type MenuItem = {
-    actionUrl?: MenuItemUrlAction | undefined;
-    actionFunction?: EventAction | undefined;
-    actionFunctionUrl?: MenuItemFunctionUrlAction | undefined;
-    actionMenu?: MenuItem[] | undefined;
-    className?: string | undefined;
-    icon?: Element | undefined;
-    isAlternatePath?: boolean | undefined;
-    isDivider?: boolean | undefined;
-    isSelected?: boolean | undefined;
-    title: string;
-  };
-
-  export type PopupMenu = {
-    className?: string | undefined;
-    menuItems: MenuItem[];
-    title: string;
-  };
-
-  export type Badge = {
-    className?: string | undefined;
-    label: string;
-    value?: number | undefined;
-  };
-
-  export type ActionItem = {
-    actionFunction?: EventAction | undefined;
-    actionPopupMenu?: PopupMenu | undefined;
-    actionDom?: (() => HTMLElement | string) | undefined;
-    className?: string | undefined;
-    badge?: Badge | undefined;
-    icon: HTMLElement | string;
-    mobileMenuLocation?: "none" | "left" | "right" | undefined;
-    showTitle: boolean;
-    title: string;
-  };
-
-  export type FooterSettings = {
-    copyrightYear?: string | null | undefined;
-    domLocationTarget?: DomLocationTarget | undefined;
-    showHorizontalRule?: boolean | undefined;
-  };
-
-  export type Logo = {
-    element?: HTMLElement | (() => HTMLElement) | undefined;
-    htmlString?: string | (() => string) | undefined;
-    imageUrl?: string | (() => string) | undefined;
-  };
-
+  export type AriaHasPopupType = 'dialog' | 'grid' | 'listbox' | 'menu' | 'tree';
+  export type ChildrenMenuTypes = ('flyout' | 'inline' | 'mega-menu' | 'plain');
+  export type Environments = 'none' | 'a1' | 'a2' | 'a3' | 'custom' | 'unittest';
+  export type Events = 'utahHeaderLoaded' | 'utahHeaderUnloaded';
+  export type PopupPlacement = 'auto' | 'auto-start' | 'auto-end' | 'bottom' | 'bottom-start' | 'bottom-end' | 'left' | 'left-start' | 'left-end' | 'right' | 'right-start' | 'right-end' | 'top' | 'top-start' | 'top-end';
+  export type Size = 'SMALL' | 'MEDIUM' | 'LARGE';
+  export type UtahIdFetchStyle = 'Automatic' | 'None' | 'Provided';
   export type MainMenuItem = {
     actionUrl?: MenuItemUrlAction | undefined;
     actionFunction?: EventAction | undefined;
@@ -138,18 +34,86 @@ declare module "@utahdts/utah-design-system-header" {
     isSelected?: boolean | undefined;
     title: string;
   };
-
   export type MainMenu = {
     menuItems: MainMenuItem[];
     title: string;
+    parentMenuLinkSuffix?: string | ((menuItem: MainMenuItem | MenuItem) => string) | undefined;
   };
-
+  export type MenuItemUrlAction = {
+    url: string;
+    openInNewTab?: boolean | undefined;
+  };
+  export type MenuItemFunctionUrlAction = {
+    actionFunction: EventAction;
+    skipHandleEvent?: boolean | undefined;
+    openInNewTab?: boolean | undefined;
+    url: string;
+  };
+  export type MenuItem = {
+    actionUrl?: MenuItemUrlAction | undefined;
+    actionFunction?: EventAction | undefined;
+    actionFunctionUrl?: MenuItemFunctionUrlAction | undefined;
+    actionMenu?: MenuItem[] | undefined;
+    className?: string | undefined;
+    icon?: Element | undefined;
+    isAlternatePath?: boolean | undefined;
+    isDivider?: boolean | undefined;
+    isSelected?: boolean | undefined;
+    title: string;
+  };
+  export type PopupMenu = {
+    className?: string | undefined;
+    menuItems: MenuItem[];
+    parentMenuLinkSuffix?: string | ((menuItem: MainMenuItem | MenuItem) => string) | undefined;
+    title: string;
+  };
   export type MediaSizes = {
     mobile: number;
     tabletLandscape: number;
     tabletPortrait: number;
   };
-
+  export type PopupFocusHandlerOptions = {
+    isPerformPopup?: (() => boolean) | undefined;
+    onClick?: ((arg0: UIEvent) => void) | undefined;
+    popupPlacement?: PopupPlacement | undefined;
+    preventOnClickHandling?: boolean | undefined;
+    shouldFocusOnHover?: boolean | undefined;
+  };
+  export type RenderPopupOptions = {
+    removePopupArrow?: boolean | undefined;
+  };
+  export type RenderPopupMenuOptions = {
+    childrenMenuType: ChildrenMenuTypes;
+    removePopupArrow?: boolean | undefined;
+    parentMenuLinkSuffix?: string | ((menuItem: MainMenuItem | MenuItem) => string) | undefined;
+  };
+  export type Badge = {
+    className?: string | undefined;
+    label: string;
+    value?: number | undefined;
+  };
+  export type ActionItem = {
+    actionFunction?: EventAction | undefined;
+    actionPopupMenu?: PopupMenu | undefined;
+    actionDom?: (() => HTMLElement | string) | undefined;
+    className?: string | undefined;
+    badge?: Badge | undefined;
+    icon: HTMLElement | string;
+    mobileMenuLocation?: "none" | "left" | "right" | undefined;
+    showTitle: boolean;
+    title: string;
+  };
+  export type DomLocationTarget = {
+    cssSelector?: string | undefined;
+    element?: HTMLElement | undefined;
+    elementFunction?: (() => HTMLElement) | undefined;
+  };
+  export type GlobalEventType = {
+    globalOnClick: (e: MouseEvent) => void;
+    globalOnKeydown: (e: KeyboardEvent) => void;
+    globalOnKeyup: (e: KeyboardEvent) => void;
+  };
+  export type SettingsInput = Partial<Settings>;
   export type UserInfo = {
     authenticated: boolean;
     disabled?: boolean | null | undefined;
@@ -163,13 +127,11 @@ declare module "@utahdts/utah-design-system-header" {
     type?: string | undefined;
     username?: string | null | undefined;
   };
-
   export type UtahIdData = {
     isDefinitive: boolean | null;
     lastError: string | null;
     userInfo: UserInfo | null;
   };
-
   export type UtahIDSettings = {
     currentUser: UserInfo | undefined | null;
     onAuthChanged?: ((arg0: UtahIdData) => void | undefined) | undefined;
@@ -178,7 +140,18 @@ declare module "@utahdts/utah-design-system-header" {
     onSignOut?: ((arg0: UIEvent) => void | undefined) | undefined;
     menuItems?: MenuItem[] | undefined;
   };
-
+  export type Logo = {
+    element?: HTMLElement | (() => HTMLElement) | undefined;
+    htmlString?: string | (() => string) | undefined;
+    imageUrl?: string | (() => string) | undefined;
+  };
+  export type FooterSettings = {
+    copyrightYear?: string | null | undefined;
+    domLocationTarget?: DomLocationTarget | undefined;
+    linkPrivacyPolicy?: string | null | undefined;
+    linkTermsOfUse?: string | null | undefined;
+    showHorizontalRule?: boolean | undefined;
+  };
   export type Settings = {
     actionItems?: ActionItem[] | undefined;
     domLocationTarget?: DomLocationTarget | undefined;
