1c3f062544417935f53328bac0b2a8137564399b
Austin Haws <ahaws@utah.gov>
2024-04-16 13:23:12 -0600

    Use PopUp component for popup
    UDS-307

--- @utahdts/utah-design-system/react/components/table/TableFilterDateRangePopup.jsx
+++ @utahdts/utah-design-system/react/components/table/TableFilterDateRangePopup.jsx
@@ -1,11 +1,10 @@
 import { useCallback, useEffect, useRef } from 'react';
-import { usePopper } from 'react-popper';
 import { useImmer } from 'use-immer';
-import { popupPlacement } from '../../enums/popupPlacement';
 import { useGlobalKeyEvent } from '../../hooks/useGlobalKeyEvent';
 import { joinClassNames } from '../../util/joinClassNames';
 import { CalendarInput } from '../forms/CalendarInput/CalendarInput';
 import { DateInput } from '../forms/DateInput';
+import { Popup } from '../popups/Popup';
 import { tableConstants } from './tableConstants';
 
 /** @typedef { 'BEGIN' | 'END' } BeginEndDate */
@@ -49,27 +48,7 @@ export function TableFilterDateRangePopup({
   value,
 }) {
   const beginDateRef = useRef(/** @type {HTMLDivElement | null} */(null));
-  const popUpDivRef = useRef(/** @type {HTMLDivElement | null} */(null));
   const [currentInput, setCurrentInput] = useImmer(/** @type {BeginEndDate} */(BeginEndDates.BEGIN));
-  const { styles, attributes, update } = usePopper(
-    popperReferenceElement.current,
-    popUpDivRef.current,
-    {
-      placement: popupPlacement.BOTTOM,
-      modifiers: [
-        { name: 'offset', options: { offset: [0, 4] } },
-      ],
-    }
-  );
-
-  useEffect(
-    () => {
-      if (update) {
-        update();
-      }
-    },
-    [isPopupOpen, update]
-  );
 
   useEffect(
     () => {
@@ -89,61 +68,55 @@ export function TableFilterDateRangePopup({
   useGlobalKeyEvent({ whichKeyCode: 'Escape', onKeyUp: useCallback(() => setIsPopupOpen(false), []) });
 
   return (
-    <div
-      aria-labelledby={tableFilterDateId}
+    <Popup
+      ariaLabelledBy={tableFilterDateId}
       className={joinClassNames(
         'table-filter-date__popup',
         !isPopupOpen && 'visually-hidden'
       )}
-      style={styles.popper}
-      ref={popUpDivRef}
-      {...attributes.popper}
+      id={`${tableFilterDateId}-popup`}
+      isVisible={isPopupOpen}
+      onVisibleChange={(_, isVisible) => setIsPopupOpen(isVisible)}
+      referenceElement={popperReferenceElement}
+      role="dialog"
     >
-      {
-        isPopupOpen
-          ? (
-            <>
-              <DateInput
-                className="table-filter-date-popup__begin-date"
-                dateFormat={dateFormat}
-                hasCalendarPopup={false}
-                id={`${tableFilterDateId}__begin-date`}
-                innerRef={beginDateRef}
-                isClearable
-                label="Date Begin"
-                onChange={(newValue) => onChange(formatNewValue(BeginEndDates.BEGIN, newValue, beginDateStr, endDateStr))}
-                onClear={() => onChange(formatNewValue(BeginEndDates.BEGIN, '', beginDateStr, endDateStr))}
-                value={beginDateStr}
-                // @ts-ignore
-                onFocus={() => setCurrentInput(BeginEndDates.BEGIN)}
-              />
-              <DateInput
-                className="table-filter-date-popup__end-date"
-                dateFormat={dateFormat}
-                hasCalendarPopup={false}
-                id={`${tableFilterDateId}__end-date`}
-                isClearable
-                label="Date End"
-                onChange={(newValue) => onChange(formatNewValue(BeginEndDates.END, newValue, beginDateStr, endDateStr))}
-                onClear={() => onChange(formatNewValue(BeginEndDates.END, '', beginDateStr, endDateStr))}
-                value={endDateStr}
-                // @ts-ignore
-                onFocus={() => setCurrentInput(BeginEndDates.END)}
-              />
-              <CalendarInput
-                className="table-filter-date-popup__calendar"
-                dateFormat={dateFormat}
-                id={`${tableFilterDateId}__calendar`}
-                label={`Calendar for table filter ${currentInput === BeginEndDates.BEGIN ? 'begin' : 'end'} date`}
-                labelClassName="visually-hidden"
-                onChange={(newValue) => onChange(formatNewValue(currentInput, newValue, beginDateStr, endDateStr))}
-                showTodayButton
-                value={(currentInput === BeginEndDates.BEGIN ? beginDateStr : endDateStr) ?? ''}
-              />
-            </>
-          )
-          : null
-      }
-    </div>
+      <DateInput
+        className="table-filter-date-popup__begin-date"
+        dateFormat={dateFormat}
+        hasCalendarPopup={false}
+        id={`${tableFilterDateId}__begin-date`}
+        innerRef={beginDateRef}
+        isClearable
+        label="Date Begin"
+        onChange={(newValue) => onChange(formatNewValue(BeginEndDates.BEGIN, newValue, beginDateStr, endDateStr))}
+        onClear={() => onChange(formatNewValue(BeginEndDates.BEGIN, '', beginDateStr, endDateStr))}
+        value={beginDateStr}
+        // @ts-ignore
+        onFocus={() => setCurrentInput(BeginEndDates.BEGIN)}
+      />
+      <DateInput
+        className="table-filter-date-popup__end-date"
+        dateFormat={dateFormat}
+        hasCalendarPopup={false}
+        id={`${tableFilterDateId}__end-date`}
+        isClearable
+        label="Date End"
+        onChange={(newValue) => onChange(formatNewValue(BeginEndDates.END, newValue, beginDateStr, endDateStr))}
+        onClear={() => onChange(formatNewValue(BeginEndDates.END, '', beginDateStr, endDateStr))}
+        value={endDateStr}
+        // @ts-ignore
+        onFocus={() => setCurrentInput(BeginEndDates.END)}
+      />
+      <CalendarInput
+        className="table-filter-date-popup__calendar"
+        dateFormat={dateFormat}
+        id={`${tableFilterDateId}__calendar`}
+        label={`Calendar for table filter ${currentInput === BeginEndDates.BEGIN ? 'begin' : 'end'} date`}
+        labelClassName="visually-hidden"
+        onChange={(newValue) => onChange(formatNewValue(currentInput, newValue, beginDateStr, endDateStr))}
+        showTodayButton
+        value={(currentInput === BeginEndDates.BEGIN ? beginDateStr : endDateStr) ?? ''}
+      />
+    </Popup>
   );
 }
