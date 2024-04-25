bcc2dc3470c9843a65994d5c572d12120272ba52
Austin Haws <ahaws@utah.gov>
2024-04-25 15:17:41 -0600

    fix: Remove search close/loop from tabindex when not shown
    UDS-1559

--- @utahdts/utah-design-system-header/src/js/renderables/search/searchModal.js
+++ @utahdts/utah-design-system-header/src/js/renderables/search/searchModal.js
@@ -8,15 +8,75 @@ import { notNull } from '../../misc/notNull';
 import { renderDOMSingle } from '../../misc/renderDOMSingle';
 import { getUtahHeaderSettings } from '../../settings/getUtahHeaderSettings';
 
+/**
+ * @typedef ModalElements {
+ *  @property {HTMLElement} backdrop
+ *  @property {HTMLElement} closeButton
+ *  @property {HTMLElement} hiddenLastFocusableButton
+ *  @property {HTMLElement} searchButton
+ *  @property {HTMLElement} searchButtonWrapper
+ *  @property {HTMLInputElement} searchInput
+ *  @property {HTMLElement} searchModal
+ * }
+ */
+
+/**
+ * @param {HTMLElement} [searchModal] can provide root search modal element in case it's not rendered to DOM yet
+ * @returns {ModalElements}
+ */
+function searchModalElements(searchModal) {
+  const searchModalUse = notNull(searchModal || document.getElementById(domConstants.SEARCH__SEARCH_MODAL), `setupSearchModal: '#${domConstants.SEARCH__SEARCH_MODAL}' not found`);
+
+  const backdrop = /** @type {HTMLElement} */ (
+    notNull(searchModalUse.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_BACKDROP)), 'setupSearchModal: backdrop not found')
+  );
+
+  const closeButton = /** @type {HTMLElement} */ (
+    notNull(searchModalUse.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_CLOSE_BUTTON)), 'setupSearchModal: closeButton not found')
+  );
+
+  const searchButton = /** @type {HTMLElement} */ (
+    notNull(searchModalUse.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_BUTTON)), 'setupSearchModal: searchButton not found')
+  );
+
+  const searchButtonWrapper = /** @type {HTMLElement} */ (
+    notNull(searchModalUse.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_BUTTON_WRAPPER)), 'setupSearchModal: searchButtonWrapper not found')
+  );
+
+  const searchInput = /** @type {HTMLInputElement} */ (
+    notNull(searchModalUse.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_INPUT)), 'setupSearchModal: searchInput not found')
+  );
+
+  const hiddenLastFocusableButton = /** @type {HTMLElement} */ (
+    notNull(searchModalUse.querySelector('.search-modal__hidden-last-focusable'), 'setupSearchModal: hiddenLastFocusableButton not found')
+  );
+
+  return {
+    backdrop,
+    closeButton,
+    hiddenLastFocusableButton,
+    searchButton,
+    searchButtonWrapper,
+    searchInput,
+    searchModal: searchModalUse,
+  };
+}
+
 function closeSearchModal() {
   const searchWrapper = notNull(document.getElementById(domConstants.SEARCH__SEARCH_MODAL), 'closeSearchModal: search modal not found');
   searchWrapper.classList.add(domConstants.VISUALLY_HIDDEN);
 
+  const { closeButton, hiddenLastFocusableButton } = searchModalElements();
+
   const searchIcon = /** @type {HTMLElement} */ (document.querySelector(getCssClassSelector(domConstants.MAIN_MENU__SEARCH)));
-  if (!searchIcon) {
-    throw new Error('closSearchModal: searchIcon not found');
+
+  // only go to menu bar search icon if closing while inside search modal (may not exist if not yet rendered)
+  if (searchIcon && document.activeElement?.closest(`#${domConstants.SEARCH__SEARCH_MODAL}`)) {
+    searchIcon.focus();
   }
-  searchIcon.focus();
+
+  closeButton.tabIndex = -1;
+  hiddenLastFocusableButton.tabIndex = -1;
 }
 
 export function showSearchModal() {
@@ -24,41 +84,25 @@ export function showSearchModal() {
   searchWrapper.classList.remove(domConstants.VISUALLY_HIDDEN);
 
   // focus on search input
-  const searchInput = notNull(
-    /** @type {HTMLInputElement} */(searchWrapper.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_INPUT))),
-    'showSearchModal: searchInput not found'
-  );
+  const { searchInput, closeButton, hiddenLastFocusableButton } = searchModalElements();
   searchInput.focus();
+
+  closeButton.tabIndex = 0;
+  hiddenLastFocusableButton.tabIndex = 0;
 }
 
 export function setupSearchModal() {
   // create the search DOM
   const searchModal = renderDOMSingle(SearchModalHtml);
 
-  const backdrop = /** @type {HTMLElement} */ (searchModal.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_BACKDROP)));
-  if (!backdrop) {
-    throw new Error('setupSearchModal: backdrop not found');
-  }
-
-  const closeButton = /** @type {HTMLElement} */ (searchModal.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_CLOSE_BUTTON)));
-  if (!closeButton) {
-    throw new Error('setupSearchModal: closeButton not found');
-  }
-
-  const searchButton = /** @type {HTMLElement} */ (searchModal.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_BUTTON)));
-  if (!searchButton) {
-    throw new Error('setupSearchModal: searchButton not found');
-  }
-
-  const searchButtonWrapper = /** @type {HTMLElement} */ (searchModal.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_BUTTON_WRAPPER)));
-  if (!searchButtonWrapper) {
-    throw new Error('setupSearchModal: searchButtonWrapper not found');
-  }
-
-  const searchInput = /** @type {HTMLInputElement} */ (searchModal.querySelector(getCssClassSelector(domConstants.SEARCH__SEARCH_INPUT)));
-  if (!searchInput) {
-    throw new Error('setupSearchModal: searchInput not found');
-  }
+  const {
+    backdrop,
+    closeButton,
+    hiddenLastFocusableButton,
+    searchButton,
+    searchButtonWrapper,
+    searchInput,
+  } = searchModalElements(searchModal);
 
   // hookup events
   // button: on trigger will search
@@ -129,10 +173,6 @@ export function setupSearchModal() {
 
   // when focusing out of the search button it is the last element on the page, so chrome would bounce to the tab bar.
   // instead, this is the last focusable element so that when it gets focus, focus can be sent back to the close button.
-  const hiddenLastFocusableButton = searchModal.querySelector('.search-modal__hidden-last-focusable');
-  if (!hiddenLastFocusableButton) {
-    throw new Error('setupSearchModal: hiddenLastFocusableButton not found');
-  }
   hiddenLastFocusableButton.addEventListener('focusin', (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -142,4 +182,7 @@ export function setupSearchModal() {
       closeButton.focus();
     }
   });
+
+  // make sure tab indexes are setup
+  closeSearchModal();
 }
