4b557c613c291e5bb98485ccb64fc05374d3b334
Gaetan Grimaud <ggrimaud@utah.gov>
2024-06-11 08:38:31 -0600

    More documentation

--- utah-design-system-website/src/react/components/websiteContent/library/components/tooltips/TooltipsDocumentation.jsx
+++ utah-design-system-website/src/react/components/websiteContent/library/components/tooltips/TooltipsDocumentation.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import {
-  ICON_BUTTON_APPEARANCE, IconButton, popupPlacement, useBanner
+  ICON_BUTTON_APPEARANCE, IconButton, popupPlacement, Tab, TabGroup, TabList, TabPanel, TabPanels, useBanner
 } from '@utahdts/utah-design-system';
 import { Link } from 'react-router-dom';
 import { PreCodeForCodeString } from '../../../../preCode/PreCodeForCodeString';
@@ -10,13 +10,17 @@ import { StaticExample } from '../../../../staticExamples/StaticExample';
 import { TooltipsExampleCodeReact } from './TooltipsExampleCodeReact';
 import { TooltipsExampleProps } from './TooltipsExampleProps';
 import { TooltipsExampleRender } from './TooltipsExampleRender';
+import { TooltipsCssClassesDocumentation } from './TooltipsCssClassesDocumentation';
+import { TootltipsPropsDocumentation } from './TootltipsPropsDocumentation';
 
 export function TooltipsDocumentation() {
   const { addBanner } = useBanner();
   return (
     <div className="documentation-content">
       <h1 id="h1-top">Tooltips</h1>
-      <p className="lead-in">Tooltips are floating labels to describe or add additional information when users hover over or focus on an interactive element.  Use sparingly.</p>
+      <p className="lead-in">Tooltips are floating labels to describe or add additional information when users hover over or focus on an interactive
+        element. Use sparingly.
+      </p>
       <hr />
       <h2 id="section-example">Example</h2>
       <SandboxExample
@@ -38,25 +42,37 @@ export function TooltipsDocumentation() {
             <IconButton
               appearance={ICON_BUTTON_APPEARANCE.BORDERLESS}
               icon={(<span className="utds-icon-before-gear" aria-hidden="true" />)}
-              onClick={() => addBanner({ message: 'Triggered the gear icon button', position: 'top-right' })}
+              onClick={() => addBanner({
+                message: 'Triggered the gear icon button',
+                position: 'top-right',
+              })}
               title="Gear icon button"
             />
             <IconButton
               appearance={ICON_BUTTON_APPEARANCE.BORDERLESS}
               icon={(<span className="utds-icon-before-plus" aria-hidden="true" />)}
-              onClick={() => addBanner({ message: 'Triggered the plus icon button', position: 'top-right' })}
+              onClick={() => addBanner({
+                message: 'Triggered the plus icon button',
+                position: 'top-right',
+              })}
               title="Plus icon button"
             />
             <IconButton
               appearance={ICON_BUTTON_APPEARANCE.BORDERLESS}
               icon={(<span className="utds-icon-before-bookmark" aria-hidden="true" />)}
-              onClick={() => addBanner({ message: 'Triggered the bookmark icon button', position: 'top-right' })}
+              onClick={() => addBanner({
+                message: 'Triggered the bookmark icon button',
+                position: 'top-right',
+              })}
               title="Bookmark icon button"
             />
             <IconButton
               appearance={ICON_BUTTON_APPEARANCE.BORDERLESS}
               icon={(<span className="utds-icon-before-help" aria-hidden="true" />)}
-              onClick={() => addBanner({ message: 'Triggered the help icon button', position: 'top-right' })}
+              onClick={() => addBanner({
+                message: 'Triggered the help icon button',
+                position: 'top-right',
+              })}
               title="Help icon button"
             />
           </>
@@ -74,17 +90,27 @@ export function TooltipsDocumentation() {
       <h3 id="section-when-to-use">When to use</h3>
       <ul>
         <li><strong>Enhance Confidence</strong>: Use tooltips to increase certainty about an interaction.</li>
-        <li><strong>Interactive Elements</strong>: Use for icon-only buttons, text links (e.g. Opens in a new tab) or for a button with an associated keyboard shortcut.</li>
+        <li><strong>Interactive Elements</strong>: Use for icon-only buttons, text links (e.g. Opens in a new tab) or for a button with an associated
+          keyboard shortcut.
+        </li>
         <li><strong>Brief Descriptions</strong>: Tooltips should be short and descriptive.</li>
-        <li><strong>Insufficient Space</strong>: Use when the description content would be too much information to include inline or create too much clutter.</li>
+        <li><strong>Insufficient Space</strong>: Use when the description content would be too much information to include inline or create too much
+          clutter.
+        </li>
       </ul>
 
       <h3 id="section-when-to-use-something-else">When to use something else</h3>
       <ul className="mb-spacing">
         <li><strong>Redundant Text</strong>: Don&apos;t use tooltips to restate visible UI text.</li>
-        <li><strong>Critical Information</strong>: To communicate critical information, including errors in forms or other interaction feedback. Consider using <Link to={pageUrls.modals}>Modals</Link> or <Link to={pageUrls.notifications}>Notifications</Link>.</li>
-        <li><strong>Avoid Links and Buttons</strong>: As tooltips only surface from a hover, never include links or buttons in the copy. If your tooltip requires either of these, consider putting your content in a <Link to={pageUrls.popups}>Popup</Link>.</li>
-        <li><strong>Keep it Brief</strong>: Good tooltips contain concise and helpful information. Keep it short (we recommend no more than 5 words max). If you need more space, consider using a <Link to={pageUrls.popups}>Popup</Link>.</li>
+        <li><strong>Critical Information</strong>: To communicate critical information, including errors in forms or other interaction feedback.
+          Consider using <Link to={pageUrls.modals}>Modals</Link> or <Link to={pageUrls.notifications}>Notifications</Link>.
+        </li>
+        <li><strong>Avoid Links and Buttons</strong>: As tooltips only surface from a hover, never include links or buttons in the copy. If your
+          tooltip requires either of these, consider putting your content in a <Link to={pageUrls.popups}>Popup</Link>.
+        </li>
+        <li><strong>Keep it Brief</strong>: Good tooltips contain concise and helpful information. Keep it short (we recommend no more than 5 words
+          max). If you need more space, consider using a <Link to={pageUrls.popups}>Popup</Link>.
+        </li>
       </ul>
 
       <h3 id="section-usability">Usability guidance</h3>
@@ -92,17 +118,27 @@ export function TooltipsDocumentation() {
         <li>Tooltips are used to provide additional information when space is tight.</li>
         <li>Tooltips must be concise with one line of text.</li>
         <li>Tooltips must be accessed via hover and focus only.</li>
-        <li>Use tooltips on Interactive elements (Buttons, Links, Icon buttons) or Non-interactive elements (Icons, Abbreviations, Truncated text) — be mindful of keyboard accessibility.</li>
-        <li>Use sparingly. Tooltips innately hide information. Consider exposing it immediately without a tooltip or removing it completely. If you&apos;re building something that requires a lot of tooltips, work on clarifying the design and the language in the experience.</li>
+        <li>Use tooltips on Interactive elements (Buttons, Links, Icon buttons) or Non-interactive elements (Icons, Abbreviations, Truncated text) —
+          be mindful of keyboard accessibility.
+        </li>
+        <li>Use sparingly. Tooltips innately hide information. Consider exposing it immediately without a tooltip or removing it completely. If
+          you&apos;re building something that requires a lot of tooltips, work on clarifying the design and the language in the experience.
+        </li>
         <li>Never include any kind of error messages in a tooltip.</li>
-        <li>Tooltips have slightly different guidelines to Popups and Modals. For more information, see <Link to={pageUrls.popups}>Popup</Link> and <Link to={pageUrls.modals}>Modals</Link> guidelines.</li>
+        <li>Tooltips have slightly different guidelines to Popups and Modals. For more information, see <Link
+          to={pageUrls.popups}
+        >Popup
+        </Link> and <Link to={pageUrls.modals}>Modals</Link> guidelines.
+        </li>
         <li>If the information is crucial for a user to proceed, consider using a modal or inline notification instead.</li>
         <li>The anatomy of a tooltip is made up of a container, description and arrow.
           <ul>
             <li>They do not include a title or heading. If you need to include a title, use a Popup.</li>
           </ul>
         </li>
-        <li>A tooltip is connected to a trigger element, such as a button or icon, that shows the tooltip when the interactive trigger element is hovered or focused. (More details below.)</li>
+        <li>A tooltip is connected to a trigger element, such as a button or icon, that shows the tooltip when the interactive trigger element is
+          hovered or focused. (More details below.)
+        </li>
         <li>Tooltip arrows are positioned to any side of the container, depending on available space within the browser viewport.
           <ul>
             <li>The arrow always points to the trigger element</li>
@@ -138,23 +174,34 @@ export function TooltipsDocumentation() {
 
       <h3 id="section-behavior-and-animation" className="mb-spacing">Behavior and Animation</h3>
       <ul className="mb-spacing">
-        <li>By default, the tooltip&apos;s arrow centers to the trigger. The arrow may shift left, right, up, down depending on available space between trigger and viewport.</li>
-        <li>By default, a Tooltip drops down from the trigger point, by slide fading towards the bottom of the screen away from the trigger point.</li>
+        <li>By default, the tooltip&apos;s arrow centers to the trigger. The arrow may shift left, right, up, down depending on available space
+          between trigger and viewport.
+        </li>
+        <li>By default, a Tooltip drops down from the trigger point, by slide fading towards the bottom of the screen away from the trigger point.
+        </li>
       </ul>
 
       <h3 id="section-accessibility" className="mb-spacing">Accessibility</h3>
       <p>Preferred Method</p>
       <ul className="mb-spacing">
-        <li>Information contained in the tooltip should always be visible to the screen reader via visually hidden text, an <code>aria-label</code>, <code>aria-labelledby</code>, or <code>aria-describedby</code>.</li>
+        <li>Information contained in the tooltip should always be visible to the screen reader via visually hidden text,
+          an <code>aria-label</code>, <code>aria-labelledby</code>, or <code>aria-describedby</code>.
+        </li>
         <li>It is unnecessary to hide information from the screen reader in this instance unless it creates confusion for the user.</li>
       </ul>
 
       <p>If you must use a tooltip for a screen reader do the following:</p>
       <ul className="mb-spacing">
-        <li>Tooltips contain limited content so we can use an announcer to announce tooltip content to people using screen readers e.g. using an ARIA live region.</li>
+        <li>Tooltips contain limited content so we can use an announcer to announce tooltip content to people using screen readers e.g. using an ARIA
+          live region.
+        </li>
         <li>A tooltip&apos;s trigger should be focusable, the tooltip should show on focus and hide on blur.</li>
-        <li>Use <code>aria-hidden=&quot;true&quot;</code> to prevent screen readers from reading hidden tooltips. Change value to <code>false</code> when tooltip is visible.</li>
-        <li>Tooltip trigger element should have an <code>aria-describedby</code> attribute which references to the <code>id</code> of the associated tooltip.</li>
+        <li>Use <code>aria-hidden=&quot;true&quot;</code> to prevent screen readers from reading hidden tooltips. Change value
+          to <code>false</code> when tooltip is visible.
+        </li>
+        <li>Tooltip trigger element should have an <code>aria-describedby</code> attribute which references to the <code>id</code> of the associated
+          tooltip.
+        </li>
         <li>By default, tooltips use <code>role=&quot;tooltip&quot;</code></li>
       </ul>
 
@@ -171,6 +218,24 @@ export function TooltipsDocumentation() {
         codeRaw={`<Button class="icon-button" data-tooltip="Something"><span className="visually-hidden">Description</span></Button>
 <Tooltip id="myTooltip" aria-hidden>Description</Tooltip>`}
       />
+
+      <h2 id="section-settings-props">Settings and Props</h2>
+      <div className="documentation-content--small-text">
+        <TabGroup defaultValue="tooltips-props-css">
+          <TabList>
+            <Tab id="tooltips-props-css">CSS</Tab>
+            <Tab id="tooltips-props-react">React</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel tabId="tooltips-props-css">
+              <TooltipsCssClassesDocumentation />
+            </TabPanel>
+            <TabPanel tabId="tooltips-props-react">
+              <TootltipsPropsDocumentation />
+            </TabPanel>
+          </TabPanels>
+        </TabGroup>
+      </div>
     </div>
   );
 }
---
+++ utah-design-system-website/src/react/components/websiteContent/library/components/tooltips/TootltipsPropsDocumentation.jsx
@@ -0,0 +1,111 @@
+import { ExternalLink, TableCell, TableRow } from '@utahdts/utah-design-system';
+import { documentationTypes } from '../../../../../enums/documentationTypes';
+import { SettingsDocumentation } from '../../documentation/SettingsDocumentation';
+
+export function TootltipsPropsDocumentation() {
+  return (
+    <SettingsDocumentation type={documentationTypes.PROPS}>
+      <TableRow>
+        <TableCell><code className="primary-color">children</code></TableCell>
+        <TableCell><code>React.ReactNode</code></TableCell>
+        <TableCell><em>required</em></TableCell>
+        <TableCell>
+          The content of the tooltip.
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell><code className="primary-color">className</code></TableCell>
+        <TableCell><code>string</code></TableCell>
+        <TableCell>null</TableCell>
+        <TableCell>This css class will be added to the tooltip.</TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell><code className="primary-color">innerRef</code></TableCell>
+        <TableCell><code>HTMLDivElement</code></TableCell>
+        <TableCell>null</TableCell>
+        <TableCell>
+          This ref will be attached to the rendered <code>div</code> element wrapper allowing the parent component to interact
+          directly with the actual DOM elements.
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell><code className="primary-color">isPopperVisible</code></TableCell>
+        <TableCell>
+          <div className="props-code-wrapper">
+            <code>true</code>
+            <span> | </span>
+            <code>false</code>
+          </div>
+        </TableCell>
+        <TableCell><code>false</code></TableCell>
+        <TableCell>
+          Controlled value used to display or hide the tooltip.
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell><code className="primary-color">offset</code></TableCell>
+        <TableCell><code>[number, number]</code></TableCell>
+        <TableCell><code>[0, 5]</code></TableCell>
+        <TableCell>
+          Control the tooltip distance from its anchor.<br />
+          See <ExternalLink href="https://popper.js.org/docs/v2/tutorial/#offset">official documentation</ExternalLink>.
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell><code className="primary-color">placement</code></TableCell>
+        <TableCell>
+          <div className="props-code-wrapper">
+            <code>auto</code>
+            <span> | </span>
+            <code>auto-start</code>
+            <span> | </span>
+            <code>auto-end</code>
+            <span> | </span>
+            <code>bottom</code>
+            <span> | </span>
+            <code>bottom-start</code>
+            <span> | </span>
+            <code>bottom-end</code>
+            <span> | </span>
+            <code>left</code>
+            <span> | </span>
+            <code>left-start</code>
+            <span> | </span>
+            <code>left-end</code>
+            <span> | </span>
+            <code>right</code>
+            <span> | </span>
+            <code>right-start</code>
+            <span> | </span>
+            <code>right-end</code>
+            <span> | </span>
+            <code>top</code>
+            <span> | </span>
+            <code>top-start</code>
+            <span> | </span>
+            <code>top-end</code>
+          </div>
+        </TableCell>
+        <TableCell><code>bottom</code></TableCell>
+        <TableCell>
+          How should the tooltip should be positioned from its anchor.<br />
+          See <ExternalLink href="https://popper.js.org/docs/v2/constructors/#options">official documentation</ExternalLink>.
+        </TableCell>
+      </TableRow>
+
+      <TableRow>
+        <TableCell><code className="primary-color">referenceElement</code></TableCell>
+        <TableCell><code>HTMLElement</code></TableCell>
+        <TableCell><em>required</em></TableCell>
+        <TableCell>
+          The reference from which the tooltip will toggle.
+        </TableCell>
+      </TableRow>
+    </SettingsDocumentation>
+  );
+}
