e3c9ab543df501482b9640601e54ab7e8dafc15c
Austin Haws <ahaws@utah.gov>
2024-08-13 14:58:13 -0600

    chore: Up dependencies
    UDS-1633

--- examples/utah-header/angular/src/app/app.component.spec.ts
+++ examples/utah-header/angular/src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'theme-demo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('theme-demo');
+    expect(app.title).toEqual('Angular Demo with Utah Header');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, theme-demo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular Demo with Utah Header');
   });
 });
