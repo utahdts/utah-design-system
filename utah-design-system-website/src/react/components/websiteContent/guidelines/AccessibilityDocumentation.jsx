f767ac4d8116eecc32d2c5e35b1dbc83a4775fb4
Gaetan Grimaud <ggrimaud@utah.gov>
2024-09-17 10:19:59 -0600

    UDS-1727: Update blockquotes

--- utah-design-system-website/src/react/components/websiteContent/guidelines/AccessibilityDocumentation.jsx
+++ utah-design-system-website/src/react/components/websiteContent/guidelines/AccessibilityDocumentation.jsx
@@ -52,13 +52,13 @@ export function AccessibilityDocumentation() {
         by fostering a sense of inclusivity, and ultimately enhance the user experience for everyone.
       </p>
 
-      <p className="block-quote">
+      <blockquote cite="https://www.ada.gov/notices/2024/03/08/web-rule/" className="block-quote">
         State agencies are required by statute to &quot;conform at minimum to W3C Web Content Accessibility Guidelines (WCAG) Version 2.1.
         This guide can be used to help achieve this standard.
         (<em><ExternalLink href="https://le.utah.gov/xcode/Title63A/Chapter16/63A-16-S209.html">§63A-16-209</ExternalLink> / <ExternalLink href="https://adminrules.utah.gov/public/rule/R895-14/Current%20Rules">R895-14</ExternalLink>. Access to Information Technology for Users with Disabilities.</em>)
         <br />
-        In addition, all State and local government entities are required to follow the web accessibility standards under Title II of the Americans with Disabilities Act (ADA). See <ExternalLink href="https://www.ada.gov/notices/2024/03/08/web-rule/">ada.gov</ExternalLink>.
-      </p>
+        In addition, all State and local government entities are required to follow the web accessibility standards under Title II of the Americans with Disabilities Act (ADA). See <cite><ExternalLink href="https://www.ada.gov/notices/2024/03/08/web-rule/">ada.gov</ExternalLink></cite>.
+      </blockquote>
 
       <hr />
       <h2 id="section-accessibility-checklist" className="text-center mt-spacing">Accessibility Checklist</h2>
