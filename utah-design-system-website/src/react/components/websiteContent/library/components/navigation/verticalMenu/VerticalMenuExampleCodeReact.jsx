7861afc661930896368a23cfe122d18a180d0e85
Joseph Sharp <jmsharp@utah.gov>
2024-09-18 16:30:38 -0600

    Show better vertical menu react code example
    UDS-1702

--- utah-design-system-website/src/react/components/websiteContent/library/components/navigation/verticalMenu/VerticalMenuExampleCodeReact.jsx
+++ utah-design-system-website/src/react/components/websiteContent/library/components/navigation/verticalMenu/VerticalMenuExampleCodeReact.jsx
@@ -1,7 +1,45 @@
-export function VerticalMenuExampleCodeReact() {
+import { SandboxIndent } from '../../../../../sandbox/SandboxIndent';
+
+/** @typedef {import('utah-design-system-website').VerticalMenuExamplePropsShape} VerticalMenuExamplePropsShape */
+
+/**
+ * @param {object} props
+ * @param {{props: VerticalMenuExamplePropsShape}} props.state
+ * @returns {import('react').JSX.Element}
+ */
+export function VerticalMenuExampleCodeReact({
+  state: {
+    props: {
+      childrenMenuType,
+    },
+  },
+}) {
   return (
     <>
-      &lt;VerticalMenu menus=&#123;menus&#125;/&gt;
+      &lt;VerticalMenu
+      <br />
+      <SandboxIndent indentLevel={1} />
+      menus=&#123;
+      <br />
+      <SandboxIndent indentLevel={2} />
+      {`[{
+      header: 'Menu Header',
+      id: 'menu-header-id',
+      menuItems: [
+        {
+          id: 'menu-item-id',
+          title: 'Menu Item',
+          children: [...children...],
+          childrenMenuType: '${childrenMenuType}',
+        },
+      ],
+    }]`}
+      <br />
+      <SandboxIndent indentLevel={1} />
+      &#125;
+      <br />
+      <SandboxIndent indentLevel={0} />
+      /&gt;
     </>
   );
 }
