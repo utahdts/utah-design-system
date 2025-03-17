fc4f4df2db7c71655a629ea362bb8b20131c4f5f
Austin Haws <ahaws@utah.gov>
2025-03-17 11:09:00 -0600

    fix: Make UtahId button behave same w/ mobile
    The non-mobile button handled the following config
    correctly where it would not sign in on click, but if
    the user is already signed in, then on mobile the UtahId button would still send the user to Utah ID:
    ```utahId: {onSignOut: (e) => window.location.href = 'https://id.utah.gov/logout'}```
    
    UDS-1955

--- @utahdts/utah-design-system-header/src/js/renderables/mobile/hookupUtahIdInMobileMenu.js
+++ @utahdts/utah-design-system-header/src/js/renderables/mobile/hookupUtahIdInMobileMenu.js
@@ -1,7 +1,5 @@
 import { domConstants, getCssClassSelector } from '../../enumerations/domConstants';
-import { utahIdUrls } from '../../enumerations/utahIdUrls';
-import { getUtahHeaderSettings } from '../../settings/getUtahHeaderSettings';
-import { getCurrentUtahIdData } from '../../utahId/utahIdData';
+import { doUtahIdOnSignIn } from '../../renderables/utahId/utahIdCommon';
 import { addMobileMenuContentItem } from './addMobileMenuContentItem';
 import { mobileMenuInteractionHandler } from './mobileMenuInteractionHandler';
 
@@ -56,29 +54,8 @@ export function hookupUtahIdInMobileMenu(mobileMenuWrapper, utahIdPopup) {
     {
       ariaHasPopupType: 'menu',
       onClickHandler: (e) => {
-        const currentUtahIdData = getCurrentUtahIdData();
-        const settings = getUtahHeaderSettings();
-        let result = false;
-        const onSignIn = (settings.utahId !== false && settings.utahId !== true && settings.utahId?.onSignIn);
-
-        if (
-          // check if controlled by app and not logged in
-          (settings.utahId !== false && settings.utahId !== true && !settings.utahId?.currentUser?.authenticated)
-          // check if not controlled by app and not logged in (definitive is true while ajax request in flight)
-          || (settings.utahId === true && (!currentUtahIdData?.isDefinitive || !currentUtahIdData?.userInfo?.authenticated))
-        ) {
-          result = true;
-          if (onSignIn) {
-            // custom onSignIn passed in settings
-            onSignIn(e);
-          } else {
-            // generic default sign method
-            e.preventDefault();
-            e.stopPropagation();
-            window.location.href = utahIdUrls.SIGN_IN;
-          }
-        }
-        return result;
+        doUtahIdOnSignIn(e);
+        return false;
       },
       shouldOnClickCloseMenu: true,
     }
