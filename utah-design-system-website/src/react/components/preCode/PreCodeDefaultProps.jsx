be57f9adcab0a5034999c34050dc058101e092fc
Gaetan Grimaud <ggrimaud@utah.gov>
2025-06-30 12:48:36 -0600

    Remove unused files

--- utah-design-system-website/src/react/components/preCode/PreCodeDefaultProps.jsx
+++
@@ -1,8 +0,0 @@
-export const PreCodeDefaultProps = {
-  addHorizontalPadding: false,
-  allowScrollOverflow: false,
-  className: '',
-  maxHeight: null,
-  propsForPre: {},
-  showBackgroundColor: false,
-};
